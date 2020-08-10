06af4241f02e9f7d08361463f67be55a9be663db
mapeee <marcus_peter@gmx.de>
2020-08-10 17:08:11 +0200

    many big changes. integrated different change into clauses

--- 01Netzaufbereitung/OEV_Aenderungen.py
+++ 01Netzaufbereitung/OEV_Aenderungen.py
@@ -15,22 +15,38 @@
 #---Vorbereitung---#
 import win32com.client.dynamic
 import time
+start_time = time.time()
+from datetime import datetime
 import random
-start_time = time.clock() ##Ziel: am Ende die Berechnungsdauer ausgeben
 
 from pathlib import Path
 path = Path.home() / 'python32' / 'python_dir.txt'
 f = open(path, mode='r')
 for i in f: path = i
-path = Path.joinpath(Path(r'C:'+path),'VISUM_Tools','Knotennummern.txt')
+path = Path.joinpath(Path(path),'VISUM_Tools','Nummern.txt')
 f = path.read_text()
 f = f.split('\n')
 
 #--Eingabe-Parameter--#
-Netz = 'C:'+f[0]
+Netz = f[0]
+Name_LineRoute = 0
+Name_Line = 0
+ValidDay = 0
+VehicleJourney = 1
+
+
+#--outputs--#
+date = datetime.now()
+txt_name = 'OEV_Aenderungen_'+date.strftime('%m%d%Y')+'.txt'
+f = open(Path.home() / 'Desktop' / txt_name,'w+')
+f.write(time.ctime()+"\n")
+f.write("Input Network: "+Netz+"\n")
+
+f.write("\n\n\n\n")
+
 
 #--VISUM öffnen--#
-VISUM = win32com.client.dynamic.Dispatch("Visum.Visum.17")
+VISUM = win32com.client.dynamic.Dispatch("Visum.Visum.20")
 VISUM.loadversion(Netz)
 VISUM.Filters.InitAll()
 
@@ -67,132 +83,113 @@ VISUM.Filters.InitAll()
 ##                    Route.SetAttValue("Name",str(int(Routenname)+random.randrange(10,800)))
 ##                    Route.Line = Name_neu
 """
-#Linienrouten
+#Lineroutes
 """
-#--Namen Linienrouten ändern--#
-##l = [] ##erstelle Liste mit allen Linien, damit keine Doppelschleifen nach dem Ändern des Namens entstehen
-##for Linien in VISUM.Net.Lines: ##Schleife über alle Linien
-##    if Linien.AttValue("TSysCode") not in ["BUS","AST","Fuss"]: continue
-##    Name = Linien.AttValue("Name")
-##    l.append((Name))
-##
-##for i in l:
-##    Linien = VISUM.Net.Lines.ItemByKey(i)
-##
-##    for Routen in Linien.LineRoutes: ##Schleife über alle Linienrouten dieser Linie
-##        Start = Routen.AttValue("StartLineRouteItem\StopPoint\Name")
-##        Ende = Routen.AttValue("EndLineRouteItem\StopPoint\Name")
-##        Nr = Routen.AttValue("Name") ##Nummer aus den Hafas-Daten
-##        Neu = Start+"--"+Ende+"("+Nr+")"
-##        Routen.SetAttValue("Name",Neu)
-
-##        for Knoten in Routen.LineRouteItems:
-##            print Knoten.AttValue("NodeNo")
-
-
-"""
-#Linien
-"""
-##VISUM.Filters.InitAll() ##Um jeweils die längste Linienroute zu erhalten!
-##Linien = VISUM.Filters.LineGroupFilter()
-##Linien.UseFilterForLineRoutes = True ##Wirksam für Fahrplanfahrt-Abschnitte
-##Linien = Linien.LineRouteFilter()
-##Linien.AddCondition("OP_NONE",False,"Length","EqualAtt","Line\Max:LineRoutes\Length")##Kein weiterer Filter,nicht komplementär,Szenario,GleichWert,Wert=1
-###--Liniennamen--#
-##l = [] ##erstelle Liste mit allen Linien, damit keine Doppelschleifen nach dem Ändern des Namens entstehen
-##for Linien in VISUM.Net.Lines: ##Schleife über alle Linien
-##    if Linien.AttValue("TSysCode") in ["BUS","AST","Fuss"]: continue
-##    Name = Linien.AttValue("Name")
-##    l.append((Name))
-##
-##for i in l:
-##    Line = VISUM.Net.Lines.ItemByKey(i)
-##    Name = Line.AttValue("Name")
-##    Vsys = Line.AttValue("TSysCode")
-##    if Vsys in ("U","S","RV","W","FV"):
-##        continue
-##
-##    print Name
-##    Betr = Line.AttValue("Operator\Name")
-##    Betr = Betr.split(" ")[0] ##Um den ganzen Namen abzutrennen.
-##    if "STB" == Vsys:
-##        Betr = Betr+"(STB)"
-##    if "AST" == Vsys:
-##        Betr = Betr+"(AST)"
-##    if "ALT" == Vsys:
-##        Betr = Betr+"(ALT)"
-##    try:
-##        Neu = Betr+"--"+Name.split("_")[1]
-##    except:
-##        Neu = Betr+"--"+Name.split("_")[0]
-##    Neu = Name.split(" ")[0]
-##    Neu = Line.AttValue("Test")
-##
-##
-##
-##    Neu += " ("+Line.AttValue("MinActive:LineRoutes\StartLineRouteItem\StopPoint\Name")+"--"+Line.AttValue("MaxActive:LineRoutes\EndLineRouteItem\StopPoint\Name")+")"
-##    print Neu
-##
-##    try:
-##        Line.SetAttValue("Name",Neu)
-##    except:
-##        print "Fehler!!!"
-##
-##VISUM.Filters.InitAll() ##Damit dieser Filter später keine Relevanz mehr hat
+if Name_LineRoute == 1:
+    f.write("LineRoute changes \n")
+    l = [] ##list of lines to avoid references to already changed attributes
+    for Linien in VISUM.Net.Lines: 
+        # if Linien.AttValue("TSysCode") not in ["BUS","AST","Fuss"]: continue
+        Name = Linien.AttValue("Name")
+        l.append((Name))
+    
+    for i in l:
+        Linien = VISUM.Net.Lines.ItemByKey(i)
+    
+        for Routen in Linien.LineRoutes: ##loop of lineroutes
+            Start = Routen.AttValue(r"StartLineRouteItem\StopPoint\Name")
+            Ende = Routen.AttValue(r"EndLineRouteItem\StopPoint\Name")
+            Nr = Routen.AttValue("Name")
+            Neu = Start+"--"+Ende+"("+Nr+")"
+            Routen.SetAttValue("Name",Neu)
+            f.write("lineroute: old: "+str(Nr)+" new: "+str(Neu)+"\n")
+    f.write("\n\n\n")
 
 """
-#--Fahrplanfahrten--#
+#Lines
 """
-###Verkehrstage#
-##l = [] ##erstelle Liste mit allen Tagen, damit keine Doppelschleifen nach dem Ändern des Namens entstehen
-##for Tag in VISUM.Net.ValidDaysCont: ##Schleife über alle Linien
-##    Tage = Tag.AttValue("No")
-##    l.append((Tage))
-##
-##for i in l:
-##    Tag = VISUM.Net.ValidDaysCont.ItemByKey(i)
-##    Nummer = int(Tag.AttValue("No"))
-##    if Nummer==1:continue
-##    Nummer_neu = Nummer+29000
-##    try:
-##        Tag.SetAttValue("No",Nummer_neu)
-##        print "neu" +str(Nummer_neu)
-##    except:
-##        print "alt" +str(Nummer)
-##        pass
-
-
-##for Fahrten in VISUM.Net.VehicleJourneys:
-##    VTneu = Fahrten.AttValue("AddVal1")
-##    if int(VTneu) != 2:
-##        continue
-##    for section in Fahrten.VehicleJourneySections:
-##        if section.AttValue("ValidDaysNo") != VTneu:
-##            section.SetAttValue("ValidDaysNo",VTneu)
-
-
-
-#Namen#
-##for Fahrten in VISUM.Net.VehicleJourneys:
-##    von = Fahrten.AttValue("FromStopPoint\Name")
-##    nach = Fahrten.AttValue("ToStopPoint\Name")
-##    Nr = int(Fahrten.AttValue("No")) ##Nummer aus Hafas-Daten
-##    Neu = von+"--"+nach+"("+str(Nr)+")"
-##    Fahrten.SetAttValue("Name",Neu)
-
-#Ende
+if Name_Line == 1:
+    f.write("Line changes \n")
+    VISUM.Filters.InitAll() 
+    Linien = VISUM.Filters.LineGroupFilter() ##To get the longest lineroute
+    Linien.UseFilterForLineRoutes = True ##for all timetable elements
+    Linien = Linien.LineRouteFilter()
+    Linien.AddCondition("OP_NONE",False,"Length","EqualAtt","Line\Max:LineRoutes\Length")##no more filter,not complementary,szenario,isValue,Vaklue=1
+    #--Linename--#
+    l = [] ##list of lines to avoid references to already changed attributes
+    for Linien in VISUM.Net.Lines: ##loop of lines
+        # if Linien.AttValue("TSysCode") in ["BUS","AST","Fuss"]: continue
+        Name = Linien.AttValue("Name")
+        l.append((Name))
+    
+    for i in l:
+        Line = VISUM.Net.Lines.ItemByKey(i)
+        Name = Line.AttValue("Name")
+        Vsys = Line.AttValue("TSysCode")
+        # if Vsys in ("U","S","RV","W","FV"):
+        #     continue
+        # Betr = Line.AttValue("Operator\Name")
+        # Betr = Betr.split(" ")[0]
+        try:
+            Neu = Name.split(",")[1]
+        except:
+            Neu = Name.split(",")[0]       
+        Neu += " ("+Line.AttValue(r"MinActive:LineRoutes\StartLineRouteItem\StopPoint\Name")+"--"+Line.AttValue(r"MaxActive:LineRoutes\EndLineRouteItem\StopPoint\Name")+")"
+ 
+        try:
+            Line.SetAttValue("Name",Neu)
+            f.write("line: old: "+str(Name)+" new: "+str(Neu)+"\n")  
+        except:
+            f.write("!!Error: line: old: "+str(Name)+" new: "+str(Neu)+"\n")
+            print ("error "+Neu)
+    
+    VISUM.Filters.InitAll() ##Damit dieser Filter später keine Relevanz mehr hat
+    f.write("\n\n\n")
 
 """
-#--Haltestellenbereiche--#
+#--ValidDays--#
 """
-##for hstbereich in VISUM.Net.Stops:
-##    neu = int(hstbereich.AttValue("Min:StopAreas\No"))
-##    alt = int(hstbereich.AttValue("No"))
-##    if neu != alt:
-##        hstbereich.SetAttValue("No",neu)
-##        print "alt :"+str(alt)+" neu: "+str(neu)
+if ValidDay == 1:
+    f.write("ValidDay changes \n")
+    l = [] ##list of lines to avoid references to already changed attributes
+    for Tag in VISUM.Net.ValidDaysCont: ##loop of lines
+        Tage = Tag.AttValue("No")
+        l.append((Tage))
+    
+    for i in l:
+        Tag = VISUM.Net.ValidDaysCont.ItemByKey(i)
+        Nummer = int(Tag.AttValue("No"))
+        if Nummer==1:continue
+        Nummer_neu = Nummer+29000
+        try:
+            Tag.SetAttValue("No",Nummer_neu)
+            f.write("ValidDay: old: "+str(Nummer)+" new: "+str(Nummer_neu)+"\n")  
+        except:pass   
+    
+    for Fahrten in VISUM.Net.VehicleJourneys:
+        VTneu = Fahrten.AttValue("AddVal1")
+        for section in Fahrten.VehicleJourneySections:
+            if section.AttValue("ValidDaysNo") != VTneu:
+                section.SetAttValue("ValidDaysNo",VTneu)          
+    f.write("\n\n\n")
 
+"""
+#--VehicleJourneys--#
+"""
+if VehicleJourney == 1:
+    f.write("VehhicleJourney changes \n")
+    for Fahrten in VISUM.Net.VehicleJourneys:
+        von = Fahrten.AttValue(r"FromStopPoint\Name")
+        nach = Fahrten.AttValue(r"ToStopPoint\Name")
+        Nr = int(Fahrten.AttValue("No")) ##Nummer aus Hafas-Daten
+        Neu = von+"--"+nach+"("+str(Nr)+")"
+        f.write("VehicleJourney: old: "+str(Fahrten.AttValue("Name")+" new: "+str(Neu)+"\n"))      
+        Fahrten.SetAttValue("Name",Neu)
+    f.write("\n\n\n")
 
-Sekunden = int(time.clock() - start_time)
+##end
+Sekunden = int(time.time() - start_time)
+print("--finished after ",Sekunden,"seconds--")
 
-print "--Scriptdurchlauf erfolgreich nach",Sekunden,"Sekunden!--"
+f.write("--finished after "+str(Sekunden)+" seconds--")
+f.close()
