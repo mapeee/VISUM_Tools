9f3f1c05e8dc28e59ce0b2620b45dc6920ea9820
mapeee <marcus_peter@gmx.de>
2020-08-18 17:06:25 +0200

    Integrated starting and ending VISUM-Session

--- 03Tkinter/Anpassungen.py
+++ 03Tkinter/Anpassungen.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Jul 30 09:24:56 2020
 
-@author: peter
+@author: mape
 """
 import win32com.client.dynamic
 import tkinter as tk
@@ -17,43 +17,59 @@ class MainApplication(tk.Frame):
         
         ##connection to path
         self.f = open(Path.home() / 'python32' / 'python_dir.txt', mode='r')
-        self.path = Path.joinpath(Path(self.f.readline()),'VISUM_Tools','Anpassungen.txt')
+        self.path = Path.joinpath(Path(self.f.readline()),'VISUM_Tools','Tkinter.txt')
         self.f = self.path.read_text().split('\n')
         
+        ##menubar
+        self.menubar = tk.Menu(self.master, bg='white')
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label = "Save as...")
+        self.menubar.add_cascade(label = "File", menu = self.filemenu)
+        self.menubar.add_cascade(label = "Help", menu = self.filemenu)
+        self.master.config(menu = self.menubar)
+        
         ##master window        
-        self.master.title("Anpassungen an VISUM-Netzen")
+        self.master.title("Changes to VISUM networks")
+        self.master.geometry("520x200")
              
         ##frame1
         self.fr1 = tk.Frame(self.master)
         tk.Label(self.fr1,text = "VISUM", pady=10, relief = "groove", font='Helvetica 12 bold').grid(
-            row=0, column=0, pady=3, sticky=("N", "S", "E", "W")) ##geometrymanager grid
-        tk.Label(self.fr1,text = "X", pady=10, relief = "groove", font='Helvetica 12 bold').grid(
-            row=0, column=1, pady=3, sticky=("N", "S", "E", "W")) ##geometrymanager grid
-        tk.Label(self.fr1,text = "Y", pady=10, relief = "groove", font='Helvetica 12 bold').grid(
-            row=0, column=2, pady=3, sticky=("N", "S", "E", "W")) ##geometrymanager grid
+            row=0, column=0, pady=3, columnspan=3, sticky=("N", "S", "E", "W")) ##geometrymanager grid
         
         #entry
-        self.ent1 = tk.Entry(self.fr1,font=40)
+        self.ent1 = tk.Entry(self.fr1, font=40, width = 40)
         self.ent1.grid(row=1,column=0)
         
         #button
-        tk.Button(self.fr1, text="browse", command = self.VISUM_file).grid(
-            row=1, column=1)
-        tk.Button(self.fr1, text="close", command = self.end).grid(
+        tk.Button(self.fr1, text="browse", font='Helvetica 8 bold', command = self.open_file).grid(
+            row=1, column=1, padx=5, pady=5)
+        tk.Button(self.fr1, text="start", font='Helvetica 8 bold', command = self.VISUM_start).grid(
+            row=1, column=2, padx=5, pady=5)
+        tk.Button(self.fr1, text="close \n VISUM", font='Helvetica 8 bold', command = self.VISUM_end).grid(
             row=2, column=1, sticky="wens", padx=5, pady=5)
+        tk.Button(self.fr1, text="end()", font='Helvetica 8 bold', command = self.end).grid(
+            row=3, column=1, sticky="wens", padx=5, pady=5)
         
         #pack Frame 1
         self.fr1.grid(row=0, column=0, padx=5, pady=5)
 
 
-    def VISUM_file(self):
+    def open_file(self):
         try:
             self.dat_in = fd.askopenfilename(initialdir = self.f[0],title = "Dateiauswahl",
                 filetypes = [("Versionsdatei", ".ver")])
             self.ent1.insert(tk.END, self.dat_in)
         except IOError: pass
     
-    
+    def VISUM_start (self):
+        self.VISUM = win32com.client.dynamic.Dispatch("Visum.Visum.20")
+        try: self.VISUM.loadversion(self.dat_in) ##Netz,Szenrario,VISUM-Instanz
+        except: pass
+        
+    def VISUM_end (self):
+        del self.VISUM
+
     def end(self):
         self.master.destroy()
         
