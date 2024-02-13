f3504c99388b4f7ac1ef94fbd72aebd5d9abaa64
mapeee <mapeee@users.noreply.github.com>
2024-02-13 15:23:18 +0100

    - new: Add Standi UDA 1.0

---
+++ 06AddIns/StandiUDA/StandiUDABody.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+import wx
+from VisumPy.AddIn import AddIn, AddInState, AddInParameter
+from VisumPy.helpers import SetMulti
+_ = AddIn.gettext
+
+def Run(param):
+    V = _("Standi-Verfahren")
+    
+    comments = [["SCHIENENBONUS", _("Weighting of travel time for rail transport with 0.5")],
+                ["LAGE", _("Location in model area (PR=1, UR=3, EUR=4, UML=5)")],
+                ["PARKEN", _("Parking availability")],
+                ["BUSSPUR", _("0=no bus lane, 1=bus lane right, 3=bus lane mid, 4=Environmental route")],
+                ["KLASSE", _("Track class (based on Ras N)")],
+                ["RAUSSTATTUNG", _("Penalty for stop equipment (Standi 2016+)")],
+                ["ABSAUFSCHLAG", _("absIncrease")],
+                ["RELAUFSCHLAG", _("relIncrease")],
+                
+                ["BEL_FV_MIT", _("Volume Long-distance PT planning case")],
+                ["BEL_FV_OHNE", _("Volume Long-distance PT base case")],
+                ["BEL_MIV_MIT", _("Volume MT (car+truck+PT base Volume) planning case")],
+                ["BEL_MIV_OHNE", _("Volume MT (car+truck+PT base Volume) base case")],
+                ["BEL_OEV_MIT", _("Volume PT planning case")],
+                ["BEL_OEV_OHNE", _("Volume PT base case")]]
+    
+    if Visum.Net.UserDefinedGroups.Count > 0:
+        if V not in np.array(Visum.Net.UserDefinedGroups.GetMultiAttValues("Name"))[:,1]: Visum.Net.AddUserDefinedGroup(V)
+    else: Visum.Net.AddUserDefinedGroup(V)
+
+    # for i in param["UDA_a"]:  
+    #     TYPE, ID, Name = i.split(" : ")
+    #     if Visum.Net.Links.AttrExists(ID)==True:
+    #         if param["Replace"]==True: SetMulti(Visum.Net.Links,ID,[0]*Visum.Net.Links.CountActive,True)
+    #         continue
+    #     Visum.Net.Links.AddUserDefinedAttribute(ID, Name, Name,1)
+    #     UDA = Visum.Net.Links.Attributes.ItemByKey(ID)
+    #     UDA.UserDefinedGroup = V
+    #     UDA.Comment = comments[[x[0] for x in comments].index(ID)][1]
+
+    for i in param["UDA_m"]:
+        TYPE, ID, Name = i.split(" : ")
+        if TYPE in ["TSYS","Verkehrssysteme"]:
+            if Visum.Net.TSystems.AttrExists(ID)==True: continue
+            Visum.Net.TSystems.AddUserDefinedAttribute(ID, Name, Name,2,defval=1.0)
+            UDA = Visum.Net.TSystems.Attributes.ItemByKey(ID)
+        if TYPE in ["ZONE","Bezirke"]:
+            if Visum.Net.Zones.AttrExists(ID)==True: continue
+            if ID =="LAGE": Visum.Net.Zones.AddUserDefinedAttribute(ID, Name, Name,1)
+            else: Visum.Net.Zones.AddUserDefinedAttribute(ID, Name, Name,2,defval=1.0)
+            UDA = Visum.Net.Zones.Attributes.ItemByKey(ID)
+        if TYPE in ["LINK","Strecken"]:
+            if Visum.Net.Links.AttrExists(ID)==True: continue
+            Visum.Net.Links.AddUserDefinedAttribute(ID, Name, Name,1)
+            UDA = Visum.Net.Links.Attributes.ItemByKey(ID)
+        if TYPE in ["STOPPOINT","Haltepunkte"]:
+            if Visum.Net.StopPoints.AttrExists(ID)==True: continue
+            Visum.Net.StopPoints.AddUserDefinedAttribute(ID, Name, Name,2)
+            UDA = Visum.Net.StopPoints.Attributes.ItemByKey(ID)
+        if TYPE in ["LINEROUTEITEM","LinienRoutenElemente"]:
+            if Visum.Net.LineRouteItems.AttrExists(ID)==True: continue
+            if ID == "ABSAUFSCHLAG": Visum.Net.LineRouteItems.AddUserDefinedAttribute(ID, Name, Name,2,defval=1.8)
+            if ID == "RELAUFSCHLAG": Visum.Net.LineRouteItems.AddUserDefinedAttribute(ID, Name, Name,2,defval=0.18) 
+            UDA = Visum.Net.LineRouteItems.Attributes.ItemByKey(ID)
+        
+        UDA.UserDefinedGroup = V
+        UDA.Comment = comments[[x[0] for x in comments].index(ID)][1]
+
+    Visum.Log(20480,_("Standi-UDA: created"))
+
+if len(sys.argv) > 1:
+    addIn = AddIn()
+else:
+    addIn = AddIn(Visum)
+
+if addIn.IsInDebugMode:
+    app = wx.PySimpleApp(0)
+    Visum = addIn.VISUM
+    addInParam = AddInParameter(addIn, None)
+else:
+    addInParam = AddInParameter(addIn, Parameter)
+
+if addIn.State != AddInState.OK:
+    addIn.ReportMessage(addIn.ErrorObjects[0].ErrorMessage)
+else:
+    try:
+        defaultParam = {"UDA_m" : "...", "Items_m" : "...", "UDA_a" : "...",
+                        "Items_a" : "...", "Replace" : False}
+        param = addInParam.Check(True, defaultParam)
+        Run(param)
+    except:
+        addIn.HandleException(addIn.TemplateText.MainApplicationError)
---
+++ 06AddIns/StandiUDA/StandiUDADlg.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import wx
+import sys
+import os
+from VisumPy.AddIn import AddIn, AddInState, AddInParameter
+_ = AddIn.gettext
+# begin wxGlade: extracode
+# end wxGlade
+
+class InfoFrame(wx.Frame):
+    def __init__(self, title):
+        super(InfoFrame, self).__init__(None, id=-1, title=title, style=wx.CAPTION | wx.STAY_ON_TOP, size=(180, 180))
+        self.Centre()
+        
+        self.button = wx.Button(self, -1, _("OK"))
+        self.Bind(wx.EVT_BUTTON, self.__OnOK, self.button)
+        self.SetBackgroundColour(wx.Colour(wx.NullColour))
+        
+        self.label = wx.StaticText(self,label= _("hvv GmbH"))
+        
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.AddSpacer(10)
+        sizer.Add(self.label,0,wx.LEFT,10)
+        sizer.AddSpacer(2)
+        sizer.Add(wx.StaticText(self,label= _("Marcus Peter")),0,wx.LEFT,10)
+        sizer.Add(wx.StaticText(self,label= _("09.02.2024")),0,wx.LEFT,10)
+        sizer.AddSpacer(2)
+        sizer.Add(wx.StaticText(self,label= _("Version 1.0: Initial")),0,wx.LEFT,10)
+        sizer.AddSpacer(20)
+        sizer.Add(self.button,0,wx.ALIGN_CENTER,5)
+        sizer.AddSpacer(10)
+        self.SetSizer(sizer)
+        
+        font = wx.Font(8, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.BOLD)
+        self.label.SetFont(font)
+
+        self.Show()
+
+    def __OnOK(self,event):
+        self.Close(True)
+
+class MyDialog(wx.Dialog):
+    def __init__(self, parent, title):
+        super(MyDialog, self).__init__(parent, id=-1, title=title, style=wx.CAPTION | wx.STAY_ON_TOP)
+
+        self.__InitUI()
+        self.Centre()
+
+    def __InitUI(self):  
+        self.label1 = wx.StaticText(self, -1, _("Set existing to 0?"))
+
+        self.button1 = wx.Button(self, -1, _("Help"))
+        self.button2 = wx.Button(self, -1, _('Info'))
+        self.button3 = wx.Button(self, -1, _("OK"))
+        self.button4 = wx.Button(self, -1, _('Cancel'))
+        self.button5 = wx.Button(self, -1, _('Select all'))
+        self.button6 = wx.Button(self, -1, _('Deselect all'))
+        
+        UDA_m = [_("TSYS : SCHIENENBONUS : Rail Bonus"),
+                      _("ZONE : LAGE : Location"),
+                      _("ZONE : PARKEN : Parking Impedance"),
+                      _("LINK : BUSSPUR : Bus lane"),
+                      _("LINK : KLASSE : Track class"),
+                      _("STOPPOINT : RAUSSTATTUNG : Penalty for equipment"),
+                      _("LINEROUTEITEM : ABSAUFSCHLAG : absIncrease"),
+                      _("LINEROUTEITEM : RELAUFSCHLAG : relIncrease")]
+        UDA_a = [_("LINK : BEL_FV_MIT : Vol Long-distance PT plannig case"),
+                      _("LINK : BEL_FV_OHNE : Vol Long-distance PT base case"),
+                      _("LINK : BEL_MIV_MIT : Vol MT plannig case"),
+                      _("LINK : BEL_MIV_OHNE : Vol MT base case"),
+                      _("LINK : BEL_OEV_MIT : Vol PT planning case"),
+                      _("LINK : BEL_OEV_OHNE : Vol PT base case")]
+        self.checkList1 = wx.CheckListBox(self, -1, choices=[_(i) for i in UDA_m])
+        self.checkList2 = wx.CheckListBox(self, -1, choices=[_(i) for i in UDA_a])        
+        for i in range(len(UDA_m)): self.checkList1.SetItemBackgroundColour(i,[wx.Colour(255,227,221),
+                                                                                wx.Colour(240,240,220),wx.Colour(240,240,220),
+                                                                                wx.Colour(193,250,193),wx.Colour(193,250,193),
+                                                                                wx.Colour(252,223,255),
+                                                                                wx.Colour(216,228,255),wx.Colour(216,228,255)][i])
+        for i in range(len(UDA_a)): self.checkList2.SetItemBackgroundColour(i,wx.Colour(193,250,193))
+        
+        self.checkbox1 = wx.CheckBox(self) 
+        
+        self.Bind(wx.EVT_BUTTON, self.OnHelp, self.button1)
+        self.Bind(wx.EVT_BUTTON, self.OnInfo, self.button2)
+        self.Bind(wx.EVT_BUTTON, self.OnOK, self.button3)
+        self.Bind(wx.EVT_BUTTON, self.OnExit, self.button4)
+        self.Bind(wx.EVT_CLOSE, self.OnExit)
+        self.Bind(wx.EVT_BUTTON, self.OnSelectAll, self.button5)
+        self.Bind(wx.EVT_BUTTON, self.OnDeselectAll, self.button6)
+
+        self.__do_layout()
+        self.__set_properties()
+        
+        defaultParam = {"UDA_m" : "...", "Items_m" : "...", "UDA_a" : "...",
+                        "Items_a" : "...", "Replace" : False}
+        param = addInParam.Check(False, defaultParam)
+   
+        try: self.__initWxControlValues(param)
+        except: pass
+        
+    def __initWxControlValues(self, param):
+        self.checkList1.SetCheckedItems(param["Items_m"])
+        self.checkList2.SetCheckedItems(param["Items_a"])
+        self.checkbox1.SetValue(param["Replace"])
+   
+    def __set_properties(self):
+        self.SetTitle(_("Create Standi-UDA"))
+        self.button2.SetDefault()
+        
+    def __do_layout(self):       
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        sb1 = wx.StaticBox(self, -1, _("UDA - Manually")) 
+        sbSizer1 = wx.StaticBoxSizer(sb1, wx.VERTICAL) 
+        sb1.SetFont(wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+
+        box_1 = wx.GridBagSizer()
+        box_1.Add(self.checkList1, (0,1), (0,2), wx.EXPAND, 5)
+        sbSizer1.Add(box_1, 0, wx.ALL|wx.CENTER, 5)
+        
+        sb2 = wx.StaticBox(self, -1, _("UDA - Automatically")) 
+        sbSizer2 = wx.StaticBoxSizer(sb2, wx.VERTICAL)
+        sb2.SetFont(wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+        
+        box_2 = wx.GridBagSizer()
+        box_2.Add(self.checkList2, (0,1), (0,2))
+        box_2.Add(self.label1, (1,1), (0,0), wx.EXPAND, 0)
+        box_2.Add(self.checkbox1, (1,2), (0,0), wx.LEFT, 5)
+        sbSizer2.Add(box_2, 0, wx.ALL|wx.CENTER, 5)  
+
+        box_3 = wx.GridBagSizer()
+        box_3.Add(self.button1, (0,1), wx.DefaultSpan, wx.EXPAND, 0)
+        box_3.Add(self.button2, (0,2), wx.DefaultSpan, wx.EXPAND, 0)
+        box_3.Add(self.button3, (0,4), wx.DefaultSpan, wx.EXPAND, 0)
+        box_3.Add(self.button4, (0,5), wx.DefaultSpan, wx.EXPAND, 10)
+        box_3.Add(self.button5, (0,7), wx.DefaultSpan, wx.RIGHT, 0)
+        box_3.Add(self.button6, (0,8), wx.DefaultSpan, wx.RIGHT, 10)
+
+        vbox.Add(sbSizer1,1,wx.ALL|wx.EXPAND,10)
+        vbox.AddSpacer(10)
+        vbox.Add(sbSizer2,0,wx.ALL|wx.EXPAND,10)
+        vbox.AddSpacer(10)
+        vbox.Add(box_3)
+        vbox.AddSpacer(20)
+        self.SetSizerAndFit(vbox)
+
+        self.Layout()
+
+    def OnExit(self,event):
+        if not addIn.IsInDebugMode:
+            Terminated.set()
+        self.Destroy()
+      
+    def OnInfo(self,event):
+        title = _("Info")
+        frame = InfoFrame(title=title)
+      
+    def OnHelp(self,event):
+        try:
+            os.startfile(addIn.DirectoryPath + _("HelpStandiUDA.htm"))
+        except:
+            addIn.HandleException()
+    
+    def OnOK(self,event):
+        param, paramOK = self.setParameter()
+        
+        if len(param["UDA_m"]) > 0:
+            wx.MessageBox(_("Please fill values into all Manually UDA!"), _("Attention!") ,wx.OK | wx.ICON_INFORMATION)
+        
+        if not paramOK or len(param["UDA_m"]+param["UDA_a"]) == 0:
+            addIn.ReportMessage(_("Please select one UDA!"))
+            return
+        
+        addInParam.SaveParameter(param)
+        
+        self.OnExit(None)
+        
+    def OnSelectAll(self,event):
+        self.checkList1.SetCheckedItems(range(self.checkList1.GetCount()))
+        self.checkList2.SetCheckedItems(range(self.checkList2.GetCount()))
+        
+    def OnDeselectAll(self,event):
+        for i in range(self.checkList1.GetCount()):self.checkList1.Check(i,False)
+        for i in range(self.checkList2.GetCount()):self.checkList2.Check(i,False)
+    
+    def setParameter(self):
+        param = dict()
+        try:
+            param["UDA_m"] = self.checkList1.GetCheckedStrings()
+            param["Items_m"] = self.checkList1.GetCheckedItems()
+            param["UDA_a"] = self.checkList2.GetCheckedStrings()
+            param["Items_a"] = self.checkList2.GetCheckedItems()
+            param["Replace"] = self.checkbox1.GetValue()
+            return param, True
+        except:
+            addIn.HandleException(_("Standi-UDA, value error: "))
+            return param, False
+
+if len(sys.argv) > 1:
+    addIn = AddIn()
+else:
+    addIn = AddIn(Visum)
+
+if addIn.IsInDebugMode:
+    app = wx.PySimpleApp(0)
+    Visum = addIn.VISUM
+    addInParam = AddInParameter(addIn, None)
+else:
+    addInParam = AddInParameter(addIn, Parameter)
+
+if addIn.State != AddInState.OK:
+    addIn.ReportMessage(addIn.ErrorObjects[0].ErrorMessage)
+
+else:
+    try:
+        wx.InitAllImageHandlers()
+        dialog_1 = MyDialog(None, "")
+        app.SetTopWindow(dialog_1)
+        dialog_1.ShowModal()
+        if addIn.IsInDebugMode:
+            app.MainLoop()
+    except:
+        addIn.HandleException(addIn.TemplateText.MainApplicationError)
+        if not addIn.IsInDebugMode:
+            Terminated.set()
