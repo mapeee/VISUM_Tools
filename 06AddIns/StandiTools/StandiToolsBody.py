0f7af2351431319cffbfb2cdc5433526d0b537e9
mapeee <mapeee@users.noreply.github.com>
2024-02-23 14:17:38 +0100

    - Standi tools: New error handling and check for PuT/PrT Connector all Zones

--- 06AddIns/StandiTools/StandiToolsBody.py
+++ 06AddIns/StandiTools/StandiToolsBody.py
@@ -9,25 +9,26 @@ _ = AddIn.gettext
 
 def Run(param):
     if param["CheckCon"]:
-        CheckCon()
+        if not CheckCon(): return
     if param["CheckTSys"]:
-        if CheckTSys(): Visum.Log(20480,_("TSys Check: OK"))
+        if CheckTSys():
+            Visum.Log(20480,_("TSys Check: OK"))
+        else: return
     if param["CheckStops"]:
-        CheckStops()
-        
+        if not CheckStops(): return
     if param["SetCon"]:
-        SetCon()
+        if not SetCon(): return
     if param["SetPtt"]:
-        SetPtt()
+        if not SetPtt(): return
     if param["SetFareSystem"]:
-        SetFareSystem()
+        if not SetFareSystem(): return
 
     Visum.Log(20480,_("Standi Tools: finished"))
 
 def CheckCon():
     if Visum.Net.Zones.AttrExists("PARKEN") == False:
-        Visum.Log(12288,_("ZONE UDA 'PARKEN' is missing!"))
-        return
+        addIn.ReportMessage(_("ZONE UDA 'PARKEN' is missing!"))
+        return False
     
     PUTWALK_PRT = 0
     PUTStopArea = 0
@@ -40,16 +41,32 @@ def CheckCon():
     if PUTWALK_PRT > 0:
         Visum.Log(20480,_("%s Connector(s) are open for PUTWALK and PrT!") %(str(PUTWALK_PRT)))
     if PUTStopArea > 0:
-        Visum.Log(12288,_("%s PUT-Connectors without StopArea!") %(str(PUTStopArea)))
+        addIn.ReportMessage(_("%s PUT-Connectors without StopArea!") %(str(PUTStopArea)))
         return False
+    
+    Visum.Filters.ConnectorFilter().Init()
+    Visum.Filters.ConnectorFilter().AddCondition("OP_NONE",False,"TSYSSET",14,"OVF")
+    nPuT = len(np.unique(np.array(Visum.Net.Connectors.GetMultiAttValues("ZONENO"))[:,1]))
+    if nPuT != Visum.Net.Zones.Count:
+        addIn.ReportMessage(_("%s Zones missing PuT-Connector!") %(str(Visum.Net.Zones.Count-nPuT)))
+        return False
+    Visum.Filters.ConnectorFilter().Init()
+    Visum.Filters.ConnectorFilter().AddCondition("OP_NONE",False,"TSYSSET",14,"C,T")
+    nPrT = len(np.unique(np.array(Visum.Net.Connectors.GetMultiAttValues("ZONENO"))[:,1]))
+    if nPrT != Visum.Net.Zones.Count:
+        addIn.ReportMessage(_("%s Zones missing PrT-Connector!") %(str(Visum.Net.Zones.Count-nPrT)))
+        return False
+    
     Visum.Log(20480,_("Connector Check: OK"))
+    return True
 
 def CheckStops():
     if Visum.Net.StopPoints.AttrExists("RAUSSTATTUNG") == False:
-        Visum.Log(12288,_("StopPoints UDA 'RAUSSTATTUNG' is missing!"))
+        addIn.ReportMessage(_("StopPoints UDA 'RAUSSTATTUNG' is missing!"))
+        return False
 
     StopAreaWalkTimes = 0
-    if CheckTSys(["PUTWALK"]) == False: return
+    if CheckTSys(["PUTWALK"]) == False: return False
     for Stop in Visum.Net.Stops.GetAll:
         if Stop.StopAreas.Count <2:continue
         for StopAreafrom in Stop.StopAreas.GetAll:
@@ -57,15 +74,16 @@ def CheckStops():
                 if Stop.StopAreaTransferWalkTime(StopAreafrom.AttValue("NO"), StopAreato.AttValue("NO"), "OVF") > 1800:
                     StopAreaWalkTimes += 1
     if StopAreaWalkTimes > 0:
-        Visum.Log(12288,_("%s StopAreaTransferWalkTime(s) > 30 Minutes!") %(str(StopAreaWalkTimes)))
+        addIn.ReportMessage(_("%s StopAreaTransferWalkTime(s) > 30 Minutes!") %(str(StopAreaWalkTimes)))
+        return False
     
     StopAreaN = 0
     StopPointNo = 0
     _State = True
     for stopArea in Visum.Net.StopAreas.GetAll:
         if stopArea.AttValue("NODENO") == 0:
-            Visum.Log(12288,_("At least on StopArea is not connected to Network (Node)!"))
-            _State = False
+            addIn.ReportMessage(_("At least on StopArea is not connected to Network (Node)!"))
+            return False
         if stopArea.AttValue("NUMSTOPPOINTS") != 1:
             StopAreaN += 1
         elif stopArea.AttValue("NO") != stopArea.AttValue(r"MIN:STOPPOINTS\NO") or stopArea.AttValue("NO") != stopArea.AttValue("NODENO"):
@@ -78,28 +96,30 @@ def CheckStops():
         Visum.Log(12288,_("%s StopArea(s) not sharing same NO with StopPoint and Node!") %(str(StopPointNo)))
         _State = False
     if _State: Visum.Log(20480,_("Stops Check: OK"))
+    return True
 
 def CheckTSys(Type = ["PUT","PrT","PUTWALK"]):
     if Visum.Net.TSystems.AttrExists("SCHIENENBONUS") == False:
-        Visum.Log(12288,_("TSys UDA 'SCHIENENBONUS' is missing!"))
+        addIn.ReportMessage(_("TSys UDA 'SCHIENENBONUS' is missing!"))
+        return False
     
     for TSys in Visum.Net.TSystems.GetAll:
         if "PUT" in Type and TSys.AttValue("TYPE") == "PUT" and TSys.AttValue("CODE") not in ["Bus", "S", "U", "RV", "FV", "W"]:
-            Visum.Log(12288,_("%s TSys %s (%s) is missing!") %("PUT", TSys.AttValue("CODE"), TSys.AttValue("NAME")))
+            addIn.ReportMessage(_("%s TSys %s (%s) is missing!") %("PUT", TSys.AttValue("CODE"), TSys.AttValue("NAME")))
             return False
         if "PRT" in Type and TSys.AttValue("TYPE") == "PRT" and TSys.AttValue("CODE") not in ["B", "C", "F", "T"]:
-            Visum.Log(12288,_("%s TSys %s (%s) is missing!") %("PRT", TSys.AttValue("CODE"), TSys.AttValue("NAME")))
+            addIn.ReportMessage(_("%s TSys %s (%s) is missing!") %("PRT", TSys.AttValue("CODE"), TSys.AttValue("NAME")))
             return False
         if "PUTWALK" in Type and TSys.AttValue("TYPE") == "PUTWALK" and TSys.AttValue("CODE") != "OVF":
-            Visum.Log(12288,_("%s TSys %s (%s) is missing!") %("PUTWALK", TSys.AttValue("CODE"), TSys.AttValue("NAME")))
+            addIn.ReportMessage(_("%s TSys %s (%s) is missing!") %("PUTWALK", TSys.AttValue("CODE"), TSys.AttValue("NAME")))
             return False
     return True
 
 def SetCon():
-    if CheckTSys(["PUTWALK", "PRT"]) == False: return
+    if CheckTSys(["PUTWALK", "PRT"]) == False: return False
     if Visum.Net.Zones.AttrExists("LAGE") == False:
-        Visum.Log(12288,_("ZONE UDA 'LAGE' is missing!"))
-        return
+        addIn.ReportMessage(_("ZONE UDA 'LAGE' is missing!"))
+        return False
     
     locations = Visum.Net.Connectors.GetMultiAttValues("ZONE\LAGE",False)
     
@@ -113,9 +133,10 @@ def SetCon():
     Visum.Net.Connectors.SetMultiAttValues("T0_TSYS(T)",newPrT)
     
     Visum.Log(20480,_("Set Connector Times: OK"))
+    return True
 
 def SetPtt():
-    if CheckTSys(["PUT"]) == False: return
+    if CheckTSys(["PUT"]) == False: return False
     
     for i in ["S","U","FV","RV","W","Bus"]:
         if i in np.array(Visum.Net.TSystems.GetMultiAttValues("CODE"))[:,1] == False:
@@ -123,12 +144,12 @@ def SetPtt():
     
     for i in ["KLASSE", "BUSSPUR"]:
         if Visum.Net.Links.AttrExists(i) == False:
-            Visum.Log(12288,_("Link UDA %s is missing!") %(i))
-            return
+            addIn.ReportMessage(_("Link UDA %s is missing!") %(i))
+            return False
     for i in ["ABSAUFSCHLAG", "RELAUFSCHLAG"]:
         if Visum.Net.LineRouteItems.AttrExists(i) == False:
-            Visum.Log(12288,_("LineRouteItems UDA %s is missing!") %(i))
-            return
+            addIn.ReportMessage(_("LineRouteItems UDA %s is missing!") %(i))
+            return False
     
     Visum.Filters.InitAll()
     Lines = Visum.Filters.LineGroupFilter()
@@ -183,15 +204,16 @@ def SetPtt():
     Line.Init()
     
     Visum.Log(20480,_("Set Perceived travel times: OK"))
+    return True
     
 def SetFareSystem():
-    if CheckTSys(["PUT"]) == False: return
+    if CheckTSys(["PUT"]) == False: return False
 
     FareList = Visum.Lists.CreateFareSystemList
     FareList.AddColumn("No")
     if FareList.NumActiveElements != 2 or int(FareList.Sum(0)) != 3: 
-        Visum.Log(12288,_("TicketSystem (NO 1: Zuschlag or NO 2: Ohne) is missing!"))
-        return
+        addIn.ReportMessage(_("TicketSystem (NO 1: Zuschlag or NO 2: Ohne) is missing!"))
+        return False
         
     Visum.Filters.InitAll()
     Lines = Visum.Net.Lines.GetAll
@@ -199,6 +221,7 @@ def SetFareSystem():
         if Line.AttValue("FARESYSTEMSET") == "":Line.SetAttValue("FARESYSTEMSET",2)
         if Line.AttValue("TSYSCODE") == "FV" and int(Line.AttValue("FARESYSTEMSET")) !=1 :Line.SetAttValue("FARESYSTEMSET",1)
     Visum.Log(20480,_("Set FareSystem: OK"))
+    return True
 
 def _conTimes(_index,_length,_locations=False):
     tacegr = 0.46*pow((_length*1000*1.2),0.4) #1.2 == Umwegfaktor
