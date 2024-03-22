1bcfafbab41c7e583aa6e6debcc95d2f491c5df9
mapeee <marcus_peter@gmx.de>
2024-03-22 12:38:39 +0100

    - many chanes to AddIn StandiProcedures

--- 06AddIns/StandiProcedures/StandiProceduresBody.py
+++ 06AddIns/StandiProcedures/StandiProceduresBody.py
@@ -4,10 +4,13 @@ import numpy as np
 import sys
 import wx
 import CalVal
+import CalNKV
+import CalPuTService
 from VisumPy.AddIn import AddIn, AddInState, AddInParameter
 _ = AddIn.gettext
 
 def Run(param):
+    Visum.Filters.InitAll()
     
     operations = Visum.Procedures.Operations.GetAll
     operations_code = [o.AttValue("CODE") for o in operations]
@@ -62,7 +65,7 @@ def Run(param):
     
     if param["Val_bc"] or param["Val_pc"]:
         if param["Val_bc"]:
-            if _("Standi-Verfahren") not in np.array(Visum.Net.UserDefinedGroups.GetMultiAttValues("Name"))[:,1]:
+            if np.any(np.array(Visum.Net.UserDefinedGroups.GetMultiAttValues("Name"))[:,1] == _("Standi-Verfahren")) == False:
                 Visum.Net.AddUserDefinedGroup(_("Standi-Verfahren"))
             if "Ohnefall-Mitfall-Vergleich" not in [i.AttValue("NAME") for i in Visum.Net.TableDefinitions.GetAll]:
                 Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\base_planning_case.accdb", True)
@@ -86,9 +89,118 @@ def Run(param):
         for CODE in ["TTC","ACT","EGT","TWT","NTR","JRT","PJT","WKT","XIMP","Rsv_O","Rsv_M",
                      "SV","SFQ","IPD","IVD","Gesamtnachfrage_O"]:
             Visum.Net.Matrices.ItemsByRef('Matrix([CODE] = \"%s")'%(CODE)).RemoveAll()   
-        Visum.Log(20480,_("Delete interim steps: finished"))
+        Visum.Log(20480,_("Delete interim steps: finished!"))
+        
+    if param["NKV"] in ["GoPuT_op_bc", "GoPuT_op_pc"]:
+        for op in Visum.Procedures.Operations.GetAll:
+            if op.AttValue("OPERATIONTYPE") == 36:
+                Visum.Procedures.OperationExecutor.SetCurrentOperation(op)
+                Visum.Procedures.OperationExecutor.ExecuteCurrentOperation()
+                break
+        if not Put_op({"GoPuT_op_bc" : True, "GoPuT_op_pc" : False}[param["NKV"]]): return
+        
+    if param["NKV"] == "GoNKV":
+        if not NKV(): return
+        
+    if param["NKV"] == "GoDel_NKV":
+        Del_NKV() 
+        Visum.Log(20480,_("Deleted all NKV components!"))
+        
+    if param["NKV"] == "GoImport_NKV":
+        Import_NKV() 
+        Visum.Log(20480,_("Imported all NKV components!"))
+        
+        
+    Visum.Log(20480,_("Standi procedures: finished!"))
 
-    Visum.Log(20480,_("Standi procedures: finished"))
+def Del_NKV():
+    for i in ["Standi-NKV", "Standi-Szenario", "Standi-Energie", "Standi-Parameter"]:
+        if np.any(np.array(Visum.Net.TableDefinitions.GetMultipleAttributes(["NAME"])) == i) == True:
+            Visum.Net.RemoveTableDefinition(Visum.Net.TableDefinitions.ItemByKey(i))
+
+    for c in ["_O", "_M"]:
+        for i in ["FAHRTEN", "PERSONALK", "UKLL", "ENERGIEK", "LL"]:
+            if Visum.Net.Lines.AttrExists(i+c): Visum.Net.Lines.DeleteUserDefinedAttribute(i+c)
+        for i in ["ENERGIEV"]:
+            if Visum.Net.VehicleCombinations.AttrExists(i+c): Visum.Net.VehicleCombinations.DeleteUserDefinedAttribute(i+c)
+        for i in ["FZG", "FAHRZEUGK", "LL"]:
+            if Visum.Net.VehicleUnits.AttrExists(i+c): Visum.Net.VehicleUnits.DeleteUserDefinedAttribute(i+c)
+
+def Import_NKV():
+    if np.any(np.array(Visum.Net.UserDefinedGroups.GetMultiAttValues("Name"))[:,1] == _("Standi-Verfahren")) == False:
+        Visum.Net.AddUserDefinedGroup(_("Standi-Verfahren"))
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_NKV.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_NKVEntries.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Energie.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_EnergieEntries.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Parameter.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_ParameterEntries.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Scenario.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_ScenarioEntries.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\PuTService_bc.accdb", True)
+    Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\PuTService_pc.accdb", True)
+
+def NKV():
+    Visum.Log(20480,_("NKV Calculation: starting!"))
+    if np.any(np.array(Visum.Net.UserDefinedGroups.GetMultiAttValues("Name"))[:,1] == _("Standi-Verfahren")) == False:
+        Visum.Net.AddUserDefinedGroup(_("Standi-Verfahren"))
+    if "Standi-NKV" not in [i.AttValue("NAME") for i in Visum.Net.TableDefinitions.GetAll]:
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_NKV.accdb", True)
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_NKVEntries.accdb", True)
+    if "Standi-Energie" not in [i.AttValue("NAME") for i in Visum.Net.TableDefinitions.GetAll]:
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Energie.accdb", True)
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_EnergieEntries.accdb", True)
+    if "Standi-Parameter" not in [i.AttValue("NAME") for i in Visum.Net.TableDefinitions.GetAll]:
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Parameter.accdb", True)
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_ParameterEntries.accdb", True)
+    if "Standi-Szenario" not in [i.AttValue("NAME") for i in Visum.Net.TableDefinitions.GetAll]:
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Scenario.accdb", True)
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_ScenarioEntries.accdb", True)
+        Visum.Log(12288,_("Table 'Standi-Szenario' was created but is empty!"))
+        return False
+
+    if not CalNKV.calc(Visum, Visum.Net.TableDefinitions.ItemByKey("Standi-NKV")): return False
+    
+    Visum.Log(20480,_("NKV calculation: finished!"))
+    return True
+
+def Put_op(bc):
+    Visum.Log(20480,_("PuT Service %s: Calculating!") %({True : _("Base case"), False : _("Planning case")}[bc]))
+    for i in ["LEERMASSE", "UKLL", "UKT", "UKTKM", "EVS", "ENERGIE", "ANSCHAFFUNGSK", "THG"]:
+        if not Visum.Net.VehicleUnits.AttrExists(i):
+            Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\PuTVehAttributes.accdb", True)
+            addIn.ReportMessage(_("UDA %s for VehicleUnits is empty/mising!")%(i))
+            return False
+        
+    if "Standi-Energie" not in [i.AttValue("NAME") for i in Visum.Net.TableDefinitions.GetAll]:
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Energie.accdb", True)
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_EnergieEntries.accdb", True)
+    if "Standi-Parameter" not in [i.AttValue("NAME") for i in Visum.Net.TableDefinitions.GetAll]:
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_Parameter.accdb", True)
+        Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\Standi_ParameterEntries.accdb", True)
+        
+    v, v_db = "O", "bc"
+    if bc == False: v, v_db = "M", "pc"
+    for i in ["FZG_", "FAHRZEUGK_", "LL_"]:
+        if not Visum.Net.VehicleUnits.AttrExists(i+v):
+            Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\PuTService_"+v_db+".accdb", True)
+            break
+    for i in ["ENERGIEV_"]:
+        if not Visum.Net.VehicleCombinations.AttrExists(i+v):
+            Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\PuTService_"+v_db+".accdb", True)
+            break
+    for i in ["FAHRTEN_", "PERSONALK_", "UKLL_", "ENERGIEK_", "LL_"]:
+        if not Visum.Net.Lines.AttrExists(i+v):
+            Visum.IO.LoadAccessDatabase(addIn.DirectoryPath + "Data\\PuTService_"+v_db+".accdb", True)
+            break         
+
+    CalPuTService.VehicleUnits(Visum,bc)
+    
+    if not CalPuTService.PuTCosts(Visum,bc):
+        return False
+    
+    Visum.Log(20480,_("PuT operating values: calculated!"))
+    return True
 
 if len(sys.argv) > 1:
     addIn = AddIn()
--- 06AddIns/StandiProcedures/StandiProceduresDlg.py
+++ 06AddIns/StandiProcedures/StandiProceduresDlg.py
@@ -64,18 +64,24 @@ class MyDialog(wx.Dialog):
         self.label_Del_pc = wx.StaticText(self, -1, _("Delete interim steps"))
         self.label_PuTCon = wx.StaticText(self, -1, _("Folder to Import/Export"))
 
-        self.button_GoVal_bc = wx.Button(self, -1, _("Go"), size=(80,20), name="GoVal_bc")
-        self.button_GoDel_bc = wx.Button(self, -1, _("Go"), size=(80,20))
-        self.button_GoVal_pc = wx.Button(self, -1, _("Go"), size=(80,20), name="GoVal_pc")
-        self.button_GoDel_pc = wx.Button(self, -1, _("Go"), size=(80,20))
+        self.button_GoVal_bc = wx.Button(self, -1, _("Go"), size=(40,17), name="GoVal_bc")
+        self.button_GoDel_bc = wx.Button(self, -1, _("Go"), size=(40,17))
+        self.button_GoVal_pc = wx.Button(self, -1, _("Go"), size=(40,17), name="GoVal_pc")
+        self.button_GoDel_pc = wx.Button(self, -1, _("Go"), size=(40,17))
         self.button_SelectAll_bc = wx.Button(self, -1, _('Select all'))
         self.button_DeselectAll_bc = wx.Button(self, -1, _('Deselect all'))
         self.button_PuTCon = wx.Button(self, -1, _('Folder'), size=(80,20))
+
+        self.button_GoPuT_op_bc = wx.Button(self, -1, _("PuT Service base case"), size=(180,30), name="GoPuT_op_bc")
+        self.button_GoPuT_op_pc = wx.Button(self, -1, _("PuT Service planning case"), size=(180,30), name="GoPuT_op_pc")      
+        self.button_GoDel_NKV = wx.Button(self, -1, _("Delete \n NKV components"), size=(120,40), name="GoDel_NKV") 
+        self.button_GoImport_NKV = wx.Button(self, -1, _("Import \n NKV components"), size=(120,40), name="GoImport_NKV")
+        self.button_GoNKV = wx.Button(self, -1, _("Calculate NKV"), size=(180,30), name="GoNKV")
         
+        self.button_ExeProc = wx.Button(self, -1, _("Execute procedures"), size=(130,25), name="ExeProc")
+        self.button_ExePre = wx.Button(self, -1, _("Preset procedures"), size=(145,25), name="ExePre")
         self.button_Cancel = wx.Button(self, -1, _('Cancel'))
         self.button_Info = wx.Button(self, -1, _('Info'))
-        self.button_ExeProc = wx.Button(self, -1, _("Execute procedures"), name="ExeProc")
-        self.button_ExePre = wx.Button(self, -1, _("Preset procedures"), name="ExePre")
         self.button_Help = wx.Button(self, -1, _("Help"))
                
         self.checkbox_PrT_bc = wx.CheckBox(self, name="PrT_bc")
@@ -91,20 +97,29 @@ class MyDialog(wx.Dialog):
         
         self.txt_PuTCon = wx.TextCtrl(self,style=wx.TE_READONLY,size =(250,20), value=r"C:\Daten\Verbindungen\verb.con")
         
+        
+        self.Bind(wx.EVT_BUTTON, self.OnSelectAll_bc, self.button_SelectAll_bc)
+        self.Bind(wx.EVT_BUTTON, self.OnDeselectAll_bc, self.button_DeselectAll_bc)
         self.Bind(wx.EVT_BUTTON, self.OnCalVal, self.button_GoVal_bc)
         self.Bind(wx.EVT_BUTTON, self.OnCalVal, self.button_GoVal_pc)
         self.Bind(wx.EVT_BUTTON, self.OnDelSteps, self.button_GoDel_bc)
         self.Bind(wx.EVT_BUTTON, self.OnDelSteps, self.button_GoDel_pc)
+        
+        self.Bind(wx.EVT_BUTTON, self.OnNKV, self.button_GoPuT_op_bc)
+        self.Bind(wx.EVT_BUTTON, self.OnNKV, self.button_GoPuT_op_pc)
+        self.Bind(wx.EVT_BUTTON, self.OnNKV, self.button_GoDel_NKV)
+        self.Bind(wx.EVT_BUTTON, self.OnNKV, self.button_GoImport_NKV)
+        self.Bind(wx.EVT_BUTTON, self.OnNKV, self.button_GoNKV)
+        
+        self.Bind(wx.EVT_BUTTON, self.OnPuTCon, self.button_PuTCon)
+        
         self.Bind(wx.EVT_BUTTON, self.OnHelp, self.button_Help)
         self.Bind(wx.EVT_BUTTON, self.OnInfo, self.button_Info)
         self.Bind(wx.EVT_BUTTON, self.OnExeProc, self.button_ExeProc)
         self.Bind(wx.EVT_BUTTON, self.OnExeProc, self.button_ExePre)
         self.Bind(wx.EVT_BUTTON, self.OnExit, self.button_Cancel)
         self.Bind(wx.EVT_CLOSE, self.OnExit)
-        self.Bind(wx.EVT_BUTTON, self.OnSelectAll_bc, self.button_SelectAll_bc)
-        self.Bind(wx.EVT_BUTTON, self.OnDeselectAll_bc, self.button_DeselectAll_bc)
-        self.Bind(wx.EVT_BUTTON, self.OnPuTCon, self.button_PuTCon)
-        
+
         self.Bind(wx.EVT_CHECKBOX, self.OnCheck, self.checkbox_PrT_bc)
         self.Bind(wx.EVT_CHECKBOX, self.OnCheck, self.checkbox_PuT_bc)
         self.Bind(wx.EVT_CHECKBOX, self.OnCheck, self.checkbox_Val_bc)
@@ -122,7 +137,8 @@ class MyDialog(wx.Dialog):
         
         defaultParam = {"PrT_bc" : False, "PuT_bc" : False, "Val_bc" : False, "Del" : False, "ExeProc" : False,
                         "PrT_pc" : False, "PrT_pcput" : False, "PuT_pc" : False, "PuT_pcprt" : False, "Val_pc" : False,
-                        "PuTCon" : False, "PuTConPath" : False}
+                        "PuTCon" : False, "PuTConPath" : False,
+                        "NKV" : False}
         param = addInParam.Check(False, defaultParam)
    
     def __set_properties(self):
@@ -130,25 +146,32 @@ class MyDialog(wx.Dialog):
         
     def __do_layout(self):         
         self.button_ExeProc.SetBackgroundColour((255, 230, 200, 255))
-        
-        self.button_GoVal_bc.SetBackgroundColour((255, 243, 248))
-        self.button_GoVal_pc.SetBackgroundColour((255, 243, 248))
+        self.button_GoPuT_op_bc.SetBackgroundColour((217, 221, 255))
+        self.button_GoPuT_op_pc.SetBackgroundColour((217, 221, 255))
+        self.button_GoDel_NKV.SetBackgroundColour((255, 198, 216))
+        self.button_GoNKV.SetBackgroundColour((191, 255, 187))
         
         self.button_PuTCon.Disable() 
         self.txt_PuTCon.Disable()
         self.label_PuTCon.Disable()
         
         vbox = wx.BoxSizer(wx.VERTICAL)
-        sb1 = wx.StaticBox(self, -1, _("Base case")) 
-        sbSizer1 = wx.StaticBoxSizer(sb1, wx.VERTICAL) 
-        sb2 = wx.StaticBox(self, -1, _("Planning case")) 
-        sbSizer2 = wx.StaticBoxSizer(sb2, wx.VERTICAL) 
-        sb3 = wx.StaticBox(self, -1, _("PuT Connections")) 
-        sbSizer3 = wx.StaticBoxSizer(sb3, wx.VERTICAL) 
+        sb_exe = wx.StaticBox(self, -1, _("Execute")) 
+        sbSizer_exe = wx.StaticBoxSizer(sb_exe, wx.VERTICAL)
+        sb_bc = wx.StaticBox(self, -1, _("Base case")) 
+        sbSizer_bc = wx.StaticBoxSizer(sb_bc, wx.VERTICAL) 
+        sb_pc = wx.StaticBox(self, -1, _("Planning case")) 
+        sbSizer_pc = wx.StaticBoxSizer(sb_pc, wx.VERTICAL) 
+        sb_PuTCon = wx.StaticBox(self, -1, _("PuT Connections")) 
+        sbSizer_PuTCon = wx.StaticBoxSizer(sb_PuTCon, wx.VERTICAL)
+        sb_NKV = wx.StaticBox(self, -1, _("NKV"))
+        sbSizer_NKV = wx.StaticBoxSizer(sb_NKV, wx.VERTICAL)
         
-        sb1.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD))
-        sb2.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD))
-        sb3.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+        sb_exe.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+        sb_bc.SetFont(wx.Font(8, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+        sb_pc.SetFont(wx.Font(8, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+        sb_PuTCon.SetFont(wx.Font(8, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+        sb_NKV.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD))
         
         self.label_PrT_bc.SetFont(wx.Font(9, wx.DEFAULT, wx.DEFAULT, wx.BOLD))
         self.label_PuT_bc.SetFont(wx.Font(9, wx.DEFAULT, wx.DEFAULT, wx.BOLD))
@@ -156,60 +179,72 @@ class MyDialog(wx.Dialog):
         self.label_PrT_pcput.SetFont(wx.Font(9, wx.DEFAULT, wx.DEFAULT, wx.BOLD))
         self.label_PuT_pc.SetFont(wx.Font(9, wx.DEFAULT, wx.DEFAULT, wx.BOLD))
         self.label_PuT_pcprt.SetFont(wx.Font(9, wx.DEFAULT, wx.DEFAULT, wx.BOLD))
-         
-        box_1 = wx.GridBagSizer()
-        box_1.Add(self.label_PrT_bc, (0,0), wx.DefaultSpan, wx.TOP,5)
-        box_1.Add(self.checkbox_PrT_bc, (0,1), wx.DefaultSpan, wx.LEFT|wx.TOP, 5)
-        box_1.Add(self.label_PuT_bc, (1,0), wx.DefaultSpan, wx.TOP,5)
-        box_1.Add(self.checkbox_PuT_bc, (1,1), wx.DefaultSpan, wx.LEFT|wx.TOP, 5)
-        box_1.Add(self.label_Val_bc, (2,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
-        box_1.Add(self.checkbox_Val_bc, (2,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
-        box_1.Add(self.button_GoVal_bc, (2,2), wx.DefaultSpan, wx.TOP, 5)
-        box_1.Add(self.label_Del_bc, (3,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
-        box_1.Add(self.checkbox_Del_bc, (3,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
-        box_1.Add(self.button_GoDel_bc, (3,2), wx.DefaultSpan, wx.TOP, 5)
-        box_1.Add(self.button_SelectAll_bc, (4,0), wx.DefaultSpan, wx.TOP|wx.EXPAND,5)
-        box_1.Add(self.button_DeselectAll_bc, (4,1), (0,2), wx.TOP|wx.EXPAND,5)
-        sbSizer1.Add(box_1, 0, wx.ALL|wx.CENTER, 5)
+
+        box_bc = wx.GridBagSizer()
+        box_bc.Add(self.label_PrT_bc, (0,0), wx.DefaultSpan, wx.TOP,5)
+        box_bc.Add(self.checkbox_PrT_bc, (0,1), wx.DefaultSpan, wx.LEFT|wx.TOP, 5)
+        box_bc.Add(self.label_PuT_bc, (1,0), wx.DefaultSpan, wx.TOP,5)
+        box_bc.Add(self.checkbox_PuT_bc, (1,1), wx.DefaultSpan, wx.LEFT|wx.TOP, 5)
+        box_bc.Add(self.label_Val_bc, (2,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
+        box_bc.Add(self.checkbox_Val_bc, (2,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
+        box_bc.Add(self.button_GoVal_bc, (2,2), wx.DefaultSpan, wx.TOP, 5)
+        box_bc.Add(self.label_Del_bc, (3,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
+        box_bc.Add(self.checkbox_Del_bc, (3,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
+        box_bc.Add(self.button_GoDel_bc, (3,2), wx.DefaultSpan, wx.TOP, 5)
+        box_bc.Add(self.button_SelectAll_bc, (4,0), wx.DefaultSpan, wx.TOP|wx.EXPAND,5)
+        box_bc.Add(self.button_DeselectAll_bc, (4,1), (0,2), wx.TOP|wx.EXPAND,5)
+        sbSizer_bc.Add(box_bc, 0, wx.ALL|wx.CENTER, 5)
         
-        box_2 = wx.GridBagSizer()
-        box_2.Add(self.label_PrT_pc, (0,0), wx.DefaultSpan, wx.TOP,5)
-        box_2.Add(self.checkbox_PrT_pc, (0,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
-        box_2.Add(self.label_PrT_pcput, (1,0), wx.DefaultSpan, wx.TOP,5)
-        box_2.Add(self.checkbox_PrT_pcput, (1,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
-        box_2.Add(self.label_PuT_pc, (2,0), wx.DefaultSpan, wx.TOP,5)
-        box_2.Add(self.checkbox_PuT_pc, (2,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
-        box_2.Add(self.label_PuT_pcprt, (3,0), wx.DefaultSpan, wx.TOP,5)
-        box_2.Add(self.checkbox_PuT_pcprt, (3,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
-        box_2.Add(self.label_Val_pc, (4,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
-        box_2.Add(self.checkbox_Val_pc, (4,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
-        box_2.Add(self.button_GoVal_pc, (4,2), wx.DefaultSpan, wx.TOP, 5)
-        box_2.Add(self.label_Del_pc, (5,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
-        box_2.Add(self.checkbox_Del_pc, (5,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
-        box_2.Add(self.button_GoDel_pc, (5,2), wx.DefaultSpan, wx.TOP, 5)
-        sbSizer2.Add(box_2, 0, wx.ALL|wx.CENTER, 5)  
+        box_pc = wx.GridBagSizer()
+        box_pc.Add(self.label_PrT_pc, (0,0), wx.DefaultSpan, wx.TOP,5)
+        box_pc.Add(self.checkbox_PrT_pc, (0,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
+        box_pc.Add(self.label_PrT_pcput, (1,0), wx.DefaultSpan, wx.TOP,5)
+        box_pc.Add(self.checkbox_PrT_pcput, (1,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
+        box_pc.Add(self.label_PuT_pc, (2,0), wx.DefaultSpan, wx.TOP,5)
+        box_pc.Add(self.checkbox_PuT_pc, (2,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
+        box_pc.Add(self.label_PuT_pcprt, (3,0), wx.DefaultSpan, wx.TOP,5)
+        box_pc.Add(self.checkbox_PuT_pcprt, (3,1), wx.DefaultSpan, wx.LEFT|wx.TOP,5)
+        box_pc.Add(self.label_Val_pc, (4,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
+        box_pc.Add(self.checkbox_Val_pc, (4,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
+        box_pc.Add(self.button_GoVal_pc, (4,2), wx.DefaultSpan, wx.TOP, 5)
+        box_pc.Add(self.label_Del_pc, (5,0), wx.DefaultSpan, wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
+        box_pc.Add(self.checkbox_Del_pc, (5,1), wx.DefaultSpan, wx.LEFT|wx.TOP|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,5)
+        box_pc.Add(self.button_GoDel_pc, (5,2), wx.DefaultSpan, wx.TOP, 5)
+        sbSizer_pc.Add(box_pc, 0, wx.ALL|wx.CENTER, 5)  
 
-        box_3 = wx.GridBagSizer()
-        box_3.Add(self.label_PuTCon, (0,0),(0,2), wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
-        box_3.Add(self.button_PuTCon, (0,2),  wx.DefaultSpan, wx.TOP|wx.LEFT, 5)
-        box_3.Add(self.txt_PuTCon, (1,0), (0,3), wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
+        box_PuTCon = wx.GridBagSizer()
+        box_PuTCon.Add(self.label_PuTCon, (0,0),(0,2), wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
+        box_PuTCon.Add(self.button_PuTCon, (0,2),  wx.DefaultSpan, wx.TOP|wx.LEFT, 5)
+        box_PuTCon.Add(self.txt_PuTCon, (1,0), (0,3), wx.TOP|wx.ALIGN_CENTER_VERTICAL,5)
+        sbSizer_PuTCon.Add(box_PuTCon, 0, wx.ALL|wx.CENTER, 5) 
         
-        sbSizer3.Add(box_3, 0, wx.ALL|wx.CENTER, 5) 
+        box_Mid = wx.GridBagSizer()
+        box_Mid.Add(self.button_ExeProc, (0,0), (0,2), wx.TOP|wx.EXPAND|wx.LEFT, 5)
+        box_Mid.Add(self.button_ExePre, (0,3), wx.DefaultSpan, wx.TOP|wx.EXPAND|wx.RIGHT, 5)
+        
+        sbSizer_exe.Add(sbSizer_bc, 0, wx.ALL, 5)
+        sbSizer_exe.Add(sbSizer_pc, 0, wx.ALL, 5)
+        sbSizer_exe.Add(sbSizer_PuTCon, 0, wx.ALL, 5)
+        sbSizer_exe.Add(box_Mid, 0, wx.ALL, 5)
 
-        box_4 = wx.GridBagSizer()
-        box_4.Add(self.button_ExeProc, (0,0), (0,2), wx.TOP|wx.EXPAND|wx.LEFT, 5)
-        box_4.Add(self.button_ExePre, (0,3), wx.DefaultSpan, wx.TOP|wx.EXPAND|wx.RIGHT, 5)
-        box_4.Add(self.button_Help, (1,0), wx.DefaultSpan, wx.TOP|wx.EXPAND|wx.LEFT, 5)
-        box_4.Add(self.button_Info, (1,1), wx.DefaultSpan, wx.TOP|wx.EXPAND, 5)
-        box_4.Add(self.button_Cancel, (1,3), wx.DefaultSpan, wx.TOP|wx.EXPAND|wx.RIGHT, 5)
+        box_NKV = wx.GridBagSizer()
+        box_NKV.Add(self.button_GoPuT_op_bc, (0,0), (0,2), wx.EXPAND|wx.TOP, 5)
+        box_NKV.Add(self.button_GoPuT_op_pc, (1,0), (0,2), wx.EXPAND|wx.TOP, 5)
+        box_NKV.Add(self.button_GoDel_NKV, (3,0),  wx.DefaultSpan, wx.TOP, 5)
+        box_NKV.Add(self.button_GoImport_NKV, (3,1),  wx.DefaultSpan, wx.TOP, 5)
+        box_NKV.Add(self.button_GoNKV, (5,0), (0,2), wx.EXPAND|wx.TOP, 5)
+        sbSizer_NKV.Add(box_NKV, 0, wx.ALL|wx.CENTER, 5)
 
-        vbox.Add(sbSizer1,0,wx.LEFT|wx.RIGHT,10)
+        box_Last = wx.GridBagSizer()
+        box_Last.Add(self.button_Help, (0,0), wx.DefaultSpan, wx.TOP|wx.EXPAND, 5)
+        box_Last.Add(self.button_Info, (0,1), wx.DefaultSpan, wx.TOP|wx.EXPAND, 5)
+        box_Last.Add(self.button_Cancel, (0,3), wx.DefaultSpan, wx.TOP|wx.RIGHT, 5)
+
+        vbox.Add(sbSizer_exe,0,wx.LEFT|wx.RIGHT,10)
         vbox.AddSpacer(20)
-        vbox.Add(sbSizer2,0,wx.LEFT|wx.RIGHT,10)
+        vbox.Add(sbSizer_NKV,0,wx.LEFT|wx.RIGHT,10)
         vbox.AddSpacer(20)
-        vbox.Add(sbSizer3,0,wx.LEFT|wx.RIGHT,10)
-        vbox.AddSpacer(10)
-        vbox.Add(box_4,0,wx.CENTER,10)
+        vbox.Add(box_Last,0,wx.LEFT,7)
         vbox.AddSpacer(5)
         self.SetSizerAndFit(vbox)
 
@@ -282,16 +317,25 @@ class MyDialog(wx.Dialog):
         self.checkbox_Val_bc.SetValue(False)
         self.checkbox_Del_bc.SetValue(False)
         
+    def OnNKV(self,event):
+        param, paramOK = self.setParameter()
+        for i in param: param[i]=False
+        param["NKV"] = event.GetEventObject().GetName()
+        addInParam.SaveParameter(param)
+        self.OnExit(None)
+        
     def OnPuTCon(self,event):
         dlg = wx.DirDialog(self, _("Choose your directory to save PuT connections:"),
                            style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
         if dlg.ShowModal() == wx.ID_OK:
             pathindir = dlg.GetPath()+r"\verb.con"
             self.txt_PuTCon.SetValue(pathindir)
+            self.txt_PuTCon.SetForegroundColour(wx.BLACK)
         dlg.Destroy()
 
     def OnExeProc(self,event):
         param, paramOK = self.setParameter()
+        param["NKV"] = False
         
         if True in [param["PuT_pc"], param["PuT_pcprt"]]:
             param["PuTCon"] = True
@@ -362,8 +406,7 @@ class MyDialog(wx.Dialog):
             param["PrT_bc"] = self.checkbox_PrT_bc.GetValue()
             param["PuT_bc"] = self.checkbox_PuT_bc.GetValue()
             param["Val_bc"] = self.checkbox_Val_bc.GetValue()
-            param["Del"] = self.checkbox_Del_bc.GetValue()
-            param["Del"] = self.checkbox_Del_pc.GetValue()
+            param["Del"] = max(self.checkbox_Del_bc.GetValue(), self.checkbox_Del_pc.GetValue())
             param["PrT_pc"] = self.checkbox_PrT_pc.GetValue()
             param["PrT_pcput"] = self.checkbox_PrT_pcput.GetValue()
             param["PuT_pc"] = self.checkbox_PuT_pc.GetValue()
@@ -372,6 +415,7 @@ class MyDialog(wx.Dialog):
             param["ExeProc"] = False
             param["PuTCon"] = False
             param["PuTConPath"] = False
+            param["NKV"] = False
             return param, True
         except:
             addIn.HandleException(_("Standi Procedures, value error: "))
