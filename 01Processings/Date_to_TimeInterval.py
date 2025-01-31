b36706c9ea4d0323217eb446302ce8757e3820b6
mapeee <marcus_peter@gmx.de>
2025-01-31 13:45:15 +0100

    - loop for years, add UDAs

--- 01Processings/Date_to_TimeInterval.py
+++ 01Processings/Date_to_TimeInterval.py
@@ -12,28 +12,37 @@ Visum = win32com.client.dynamic.Dispatch("Visum.Visum.25")
 Visum.IO.loadversion(r"")
 Visum.Filters.InitAll()
 
+years = [2024]
 
-# Function to generate date strings for a given year
-def generate_date_strings(year):
-    start_date = datetime(year, 1, 1)
+
+def generate_date_strings(_year):
+    start_date = datetime(_year, 1, 1)
     date_list = []
-    for i in range(365 + (1 if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)) else 0)):
+    for i in range(365 + (1 if (_year % 4 == 0 and (_year % 100 != 0 or _year % 400 == 0)) else 0)):
         current_date = start_date + timedelta(days=i)
         date_string1 = current_date.strftime("%b_%a_%d")
         date_string2 = current_date.strftime("%d.%m.%Y")
         date_list.append([date_string1, date_string2])
     return date_list
 
-# Example usage
-year = 2024 # Change the year as needed 
-date_strings = generate_date_strings(year)
 
-# Print the first 10 dates as a sample
-TimeIntSet = Visum.Net.AddTimeIntervalSet(3)
-TimeIntSet.SetAttValue("CODE", str(year))
-TimeIntSet.SetAttValue("Name", "AFZ")
+maxinterval = max(Visum.Net.TimeIntervalSets.GetMultiAttValues("NO"))[1] + 1
 
-for i, date in enumerate(date_strings):
-    TimeInt = TimeIntSet.AddTimeInterval(date[0], i, i + 1, DayIndex=1)
-    TimeInt.SetAttValue("Name", date[1])
+for e, year in enumerate(years):
+    TimeIntNo = int(maxinterval + e)
+    TimeIntSet = Visum.Net.AddTimeIntervalSet(TimeIntNo)
+    TimeIntSet.SetAttValue("CODE", str(year))
+    TimeIntSet.SetAttValue("Name", "AFZ")
+
+    date_strings = generate_date_strings(year)
+    for i, date in enumerate(date_strings):
+        TimeInt = TimeIntSet.AddTimeInterval(date[1], i, i + 1, DayIndex=1)
+        TimeInt.SetAttValue("Name", date[0])
+    
+ 
+    for UDA in ["FahrtNr_AFZ", "Einsteiger_AFZ", "Aussteiger_AFZ", "FzgNr_AFZ"]:
+        Visum.Net.VehicleJourneyItems.AddUserDefinedAttribute(UDA, UDA, UDA, 1)
+    
+    
+
     
