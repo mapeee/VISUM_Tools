159ab29ee1802bdc654bff77219c73cc9d33e27c
mapeee <marcus_peter@gmx.de>
2025-02-04 16:18:52 +0100

    - Usage of day number of the month instead of day number of the year

--- 05Demand/AFZ/Date_to_TimeInterval.py
+++ 05Demand/AFZ/Date_to_TimeInterval.py
@@ -28,8 +28,9 @@ def generate_date_strings(_year):
         current_date = start_date + timedelta(days=i)
         date_string1 = current_date.strftime("%b_%a_%d")
         date_string2 = current_date.strftime("%d.%m.%Y")
+        day_number = current_date.day
         month_number = current_date.month
-        date_list.append([date_string1, date_string2, month_number])
+        date_list.append([date_string1, date_string2, day_number, month_number])
     return date_list
 
 
@@ -52,13 +53,20 @@ for e, year in enumerate(years):
     # create time interval for each string / day a year
     date_strings = generate_date_strings(year)
     for i, date in enumerate(date_strings):
-        TimeInt = TimeIntSet.AddTimeInterval(date[1], i + (date[2] * 3600), i + 1  + (date[2] * 3600), DayIndex=1)
+        TimeInt = TimeIntSet.AddTimeInterval(date[1], date[2] + (date[3] * 3600), date[2] + 1  + (date[3] * 3600), DayIndex = 1)
         TimeInt.SetAttValue("Name", date[0])
     
     # create additional user defined attributes to organize AFZ data
     for UDA in [f"FahrtNr_AFZ_{year}", f"Einsteiger_AFZ_{year}", f"Aussteiger_AFZ_{year}", f"FzgNr_AFZ_{year}", f"Belastung_ab_AFZ_{year}"]:
-        Visum.Net.VehicleJourneyItems.AddUserDefinedAttribute(UDA, UDA, UDA, 1, 0, DefVal = "", CanBeEmpty = True, SubAttr = f"TISET_{TimeIntNo}")
+        if Visum.Net.VehicleJourneyItems.AttrExists(UDA): continue
+        Visum.Net.VehicleJourneyItems.AddUserDefinedAttribute(UDA, UDA, UDA, 1, 0, False, "", "", "", 0, False, "", f"TISET_{TimeIntNo}", True)
         UDG = Visum.Net.VehicleJourneyItems.Attributes.ItemByKey(UDA)
+        UDG.UserDefinedGroup = "AFZ"
+    
+    UDA = f"Saldo_AFZ_{year}"
+    if not Visum.Net.VehicleJourneys.AttrExists(UDA):
+        Visum.Net.VehicleJourneys.AddUserDefinedAttribute(UDA, UDA, UDA, 1)
+        UDG = Visum.Net.VehicleJourneys.Attributes.ItemByKey(UDA)
         UDG.UserDefinedGroup = "AFZ"
         
         
