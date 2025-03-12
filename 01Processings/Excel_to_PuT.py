78fe7411769fe6f2c995d8235d389af9e11f3525
mapeee <marcus_peter@gmx.de>
2025-03-12 11:14:47 +0100

    - many changes to Excel_to_PuT

--- 01Processings/Excel_to_PuT.py
+++ 01Processings/Excel_to_PuT.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python
 
 #-------------------------------------------------------------------------------
-# Name:        PuT from Excel to PTV VISUM
+# Name:        PuT from Excel to PTV Visum
 # Author:      mape
 # Created:     08/04/2021 // 15/05/2023
 # Copyright:   (c) mape 2021
@@ -11,14 +11,20 @@
 
 import win32com.client.dynamic
 import openpyxl
-
 from pathlib import Path
-path = Path.home() / 'python32' / 'python_dir.txt'
-path = open(path, mode='r').readlines()
-path = path[0] +"\\"+'VISUM_Tools'+"\\"+'Excel_to_PuT.txt'
-f = open(path, mode='r').read().splitlines()
+f = open(Path.home() / 'python32' / 'python_dir.txt', mode='r')
+path = list(f)[0]
+path = Path.joinpath(Path(path), 'VISUM_Tools', 'Excel_to_PuT.txt')
+f = path.read_text().split('\n')
+
 Network = f[0]
 XLSX = f[1]
+
+#--Parameter--#
+LineName = "A2"
+TSys = "Bus"
+Direction = "A5"
+StopsFrom = "F18"
 
 
 def CreateHeadway(Headway, Dep, Dep_next, No, NoVJ):
@@ -26,88 +32,81 @@ def CreateHeadway(Headway, Dep, Dep_next, No, NoVJ):
     for HeadwayJourney in range(HeadwayJourneys):
         NoHead = int(str(No)+str(HeadwayJourney+1))
         DepHead = Dep + (Headway*(HeadwayJourney+1))
-        CreateVehJourney(VISUM, TimeProfile, NoHead, NoVJ, DepHead)
+        CreateVehJourney(Visum, TimeProfile, NoHead, NoVJ, DepHead)
 
-def CreateLine(VISUM, XLSX):
-    LineName = XLSX.cell(1,2).value
+def CreateLine(_Visum, _XLSX, __LineName, _TSys):
+    _LineName = _XLSX[__LineName].value
     try:
-        Line = VISUM.Net.Lines.ItemByKey(LineName)
-        print("> Line "+LineName+ " already exists")
+        Line = _Visum.Net.Lines.ItemByKey(_LineName)
+        print("> Line", _LineName, "already exists")
         return Line
     except:
-        print("-> creating Line "+LineName)
-        Line = VISUM.Net.AddLine(LineName,XLSX.cell(3,2).value)
-        Line.SetAttValue("LINIENNUMMER",XLSX.cell(2,2).value)
-        Line.SetAttValue("OPERATORNO",XLSX.cell(4,2).value)
-        Line.SetAttValue("FARESYSTEMSET",XLSX.cell(5,2).value)
+        print("> creating Line ", _LineName)
+        Line = _Visum.Net.AddLine(_LineName, _TSys)
         return Line
 
-def CreateLineRoute(VISUM, Line, No, Stops, Direct):
-    RouteName = str(No)+"_"+Direct
-    RouteSearch = VISUM_para(VISUM)
-    Route = VISUM.Net.AddLineRoute(RouteName,Line,Direct,Stops,RouteSearch)
-    Route.SetAttValue("Szenario","Script")
-    return Route
+def CreateLineRoute(_Visum, _XLSX, _Line, _Stops, _Direction):
+    if _Line.LineRoutes.Count == 0:
+        _No = 1
+    else:
+        _No = int(_Line.AttValue(r"MAX:LINEROUTES\ID")) + 1
+    _RouteName = str(_No)
+    _Direction = _XLSX[_Direction].value
+    if _Direction == "Richtung 1": _Direction = "H"
+    if _Direction == "Richtung 2": _Direction = "R"
+    if _Direction not in ["H", "R"]:
+        print("> wrong direction:", _Direction)
+        raise
+    RouteSearch = Visum_para(_Visum)
+    LineRoute = _Visum.Net.AddLineRoute(_RouteName, _Line, _Direction, _Stops, RouteSearch)
+    if LineRoute == None:
+        print("> no LineRoute added")
+        raise
+    return LineRoute
 
-def CreateTimeProfile(VISUM, Route, Times):
-    TProfile = VISUM.Net.AddTimeProfile("1",Route)
-    for Item in TProfile.TimeProfileItems:
-        Index = Item.AttValue("Index")
-        prevarr = Item.AttValue("PREVTIMEPROFILEITEM\ARR")
-        if Index==1:continue
-        arr = prevarr+Times[int(Index-2)]
-        Item.SetAttValue("arr",arr)
+def CreateTimeProfile(_Visum, _Route):
+    TProfile = _Visum.Net.AddTimeProfile("1", _Route)
+    # for Item in TProfile.TimeProfileItems:
+    #     Index = Item.AttValue("Index")
+    #     prevarr = Item.AttValue("PREVTIMEPROFILEITEM\ARR")
+    #     if Index==1:continue
+    #     arr = prevarr+Times[int(Index-2)]
+    #     Item.SetAttValue("arr",arr)
     return TProfile    
 
-def CreateVehJourney(VISUM, TimeProfile, No, NoVJ, Dep):
-    VJ = VISUM.Net.AddVehicleJourney(NoVJ,TimeProfile)
+def CreateVehJourney(_Visum, TimeProfile, No, NoVJ, Dep):
+    VJ = _Visum.Net.AddVehicleJourney(NoVJ,TimeProfile)
     VJ.SetAttValue("Dep",Dep)
     VJ.SetAttValue("Name",str(No))
     Operator = VJ.AttValue('LINEROUTE\LINE\OPERATORNO')
     VJ.SetAttValue('OPERATORNO',Operator)
     return VJ
 
-def DepartureTime(XLSX, Col):
-    for row in range(10,XLSX.max_row+10):
-        Time = XLSX.cell(row,Col).value
+def DepartureTime(_XLSX, Col):
+    for row in range(10,_XLSX.max_row+10):
+        Time = _XLSX.cell(row,Col).value
         if Time in [None,"|","S"]:continue
         Departure = Times_to_seconds(Time,None)
         break
     return Departure
 
-def Excel(path):
-    XLSX = path
-    XLSX = openpyxl.load_workbook(XLSX)
-    return XLSX
-
-def Filter(VISUM):
-    VISUM.Filters.InitAll()
-    Lines = VISUM.Filters.LineGroupFilter()
-    Lines.UseFilterForLineRoutes = True
-    Lines.UseFilterForLines = True
-    LineRouteF = Lines.LineRouteFilter()
-    LineRouteF.AddCondition("OP_NONE",False,"Szenario",9,"Script")
-
-def HeadwayCheck(Check):
-    if Check is None:return None
-    if "Min" not in str(Check) and "min" not in str(Check):return None
-    freq = int(Check[:2])*60
-    return freq  
-
-def Stop_Container(VISUM, XLSX, Col):
-    Stops = VISUM.CreateNetElements()
-    Times = []
-    Time_from = None
-    Time_to = None
-    for row in range(10,XLSX.max_row+10):
-        Stop = XLSX.cell(row,Col).value
-        if Stop in [None,"|","S"]:continue
-        Stops.Add(VISUM.Net.StopPoints.ItemByKey(XLSX.cell(row,2).value))
-        Time_to = XLSX.cell(row,Col).value
-        seconds = Times_to_seconds(Time_from, Time_to)
-        if seconds != None:Times.append(seconds)
-        Time_from = XLSX.cell(row,Col).value
-    return Stops, Times
+def Stop_Container(_Visum, _XLSX, _StopsFrom):
+    Stops = _Visum.CreateNetElements()
+    _row = _XLSX[_StopsFrom].row
+    _col = _XLSX[_StopsFrom].column
+    for row in range(_row,_XLSX.max_row+10):
+        Stop_No = _XLSX.cell(row, _col).value
+        if Stop_No == None:
+            return Stops
+        if Stop_No == _XLSX.cell(row-1, _col).value:
+            print("> Identical StopID one after the other:", Stop_No)
+            raise
+        try:
+            _Stop = _Visum.Net.StopPoints.ItemByKey(Stop_No)
+        except:
+            print("> StopID", Stop_No, "not found")
+            raise
+        Stops.Add(_Stop)
 
 def Times_to_seconds(Time_from, Time_to):
     if Time_from == None:
@@ -120,54 +119,67 @@ def Times_to_seconds(Time_from, Time_to):
     seconds = (minutes_to - minutes_from)*60
     return seconds
 
-def VISUM_open(Net):
-    VISUM = win32com.client.dynamic.Dispatch("Visum.Visum.22")
-    VISUM.loadversion(Net)
-    VISUM.Filters.InitAll()
-    return VISUM
+def Visum_open(Net):
+    _Visum = win32com.client.dynamic.Dispatch("Visum.Visum.24")
+    _Visum.loadversion(Net)
+    _Visum.Filters.InitAll()
+    return _Visum
 
-def VISUM_para(VISUM):
-    Routing = VISUM.CreateNetReadRouteSearchTsys()
-    Routing.SetAttValue("ChangeLinkTypeOfOpenedLinks",False)
-    Routing.SetAttValue("IncludeBlockedTurns",False)
-    Routing.SetAttValue("HowToHandleIncompleteRoute", 2) ##search shortest path
-    Routing.SetAttValue("LinkTypeForInsertedLinksReplacingMissingShortestPaths",1)
-    Routing.SetAttValue("ShortestPathCriterion",1) ##link length (1 = travel time; 3 = link length)
-    Routing.SetAttValue("MaxDeviationFactor",8)
-    Routing.SetAttValue("WhatToDoIfShortestPathNotFound",2) ##insert link
-    Routing.SetAttValue("WhatToDoIfStopPointIsBlocked", 0) ##do not read lineroute
-    Routing.SetAttValue("WhatToDoIfStopPointNotFound", 0) ##do not read lineroute
+def Visum_para(_Visum):
+    Routing = _Visum.IO.CreateNetReadRouteSearchTsys()
+    Routing.SetAttValue("ChangeLinkTypeOfOpenedLinks", False)
+    Routing.SetAttValue("IncludeBlockedTurns", True)
+    Routing.SetAttValue("HowToHandleIncompleteRoute", 2) # search shortest path
+    Routing.SetAttValue("LinkTypeForInsertedLinksReplacingMissingLinks", 1)
+    Routing.SetAttValue("ShortestPathCriterion", 1) # link length (1 = travel time; 3 = link length)
+    Routing.SetAttValue("MaxDeviationFactor", 50)
+    Routing.SetAttValue("WhatToDoIfShortestPathNotFound", 1) # open link / turn
+    Routing.SetAttValue("LinkTypeForOpenedLinks", 1)
+    Routing.SetAttValue("LinkTypeForInsertedLinksReplacingMissingShortestPaths", 1)
+    Routing.SetAttValue("WhatToDoIfStopPointIsBlocked", 2) # open stopPoint
+    Routing.SetAttValue("WhatToDoIfStopPointNotFound", 0) # do not read lineroute
+    
     return Routing
 
-#--Parameter--#
-VISUM = VISUM_open(Network)
-VHNo = VISUM.Net.VehicleJourneys.GetAll
-VHNoMax = VHNo[len(VHNo)-1].AttValue("No")
-VHNo = VHNoMax+1
-Direct = "R"
-Elements = Excel(XLSX)[Direct]
-Line = CreateLine(VISUM,Elements)
-No=Dep=Stops=Times=LineRoute=TimeProfile=VehJourney = None
+#--Processing--#
+Visum = Visum_open(Network)
+wb = openpyxl.load_workbook(XLSX, data_only = True)
 
-for Journey in range(3,Elements.max_column+1):
-    No = Elements.cell(9,Journey).value
-    print("> Journey: "+str(No))
-    if HeadwayCheck(Elements.cell(10,Journey).value) != None:
-        print("> Headway Journey: "+str(No))
-        Headway = HeadwayCheck(Elements.cell(10,Journey).value)
-        Dep_next = DepartureTime(Elements,Journey+1)
-        CreateHeadway(Headway, Dep, Dep_next, No, VHNo)
-        VHNo+=1
-        continue          
-    Dep = DepartureTime(Elements,Journey)
-    Stops, Times = Stop_Container(VISUM, Elements, Journey)
-    LineRoute = CreateLineRoute(VISUM, Line, No, Stops, Direct)
-    TimeProfile = CreateTimeProfile(VISUM, LineRoute, Times)
-    VehJourney = CreateVehJourney(VISUM, TimeProfile, No, VHNo, Dep)
-    VHNo+=1
-    
-Filter(VISUM)
+for Excel in wb.worksheets:
+    if Excel.title == "Tabelle1":
+        continue
+    Line = CreateLine(Visum, Excel, LineName, TSys)
+    try:
+        Stops = Stop_Container(Visum, Excel, StopsFrom)
+        LineRoute = CreateLineRoute(Visum, Excel, Line, Stops, Direction)
+        TimeProfile = CreateTimeProfile(Visum, LineRoute)
+    except:
+        print("> error")
+    print()
+
+# VHNo = int(Visum.Net.VehicleJourneys.GetMultipleAttributes(["NO"])[-1][0]) + 1
+# Direct = "R"
+
+# Elements = Excel
+
+# No=Dep=Stops=Times=LineRoute=TimeProfile=VehJourney = None
+
+# for Journey in range(7,Elements.max_column+1):
+#     # No = Elements.cell(9,Journey).value
+#     # print("> Journey: "+str(No))
+#     # if HeadwayCheck(Elements.cell(10,Journey).value) != None:
+#     #     print("> Headway Journey: "+str(No))
+#     #     Headway = HeadwayCheck(Elements.cell(10,Journey).value)
+#     #     Dep_next = DepartureTime(Elements,Journey+1)
+#     #     CreateHeadway(Headway, Dep, Dep_next, No, VHNo)
+#     #     VHNo+=1
+#     #     continue          
+#     Dep = DepartureTime(Elements,Journey)
+#     Stops, Times = Stop_Container(Visum, Elements, Journey)
+#     LineRoute = CreateLineRoute(Visum, Line, No, Stops, Direct)
+#     TimeProfile = CreateTimeProfile(Visum, LineRoute, Times)
+#     VehJourney = CreateVehJourney(Visum, TimeProfile, No, VHNo, Dep)
+#     VHNo+=1
 
 #End
-# VISUM.SaveVersion(Network)
 print("> finished")
