8d48c3550eb2f7b972ebe5d7205df93034058ad5
mapeee <marcus_peter@gmx.de>
2025-07-11 12:38:05 +0200

    - Connections_from_poi: Shares integrated

--- 01Processings/Connections_from_poi.py
+++ 01Processings/Connections_from_poi.py
@@ -7,11 +7,12 @@
 # #---------------------
 
 import pandas as pd
+from sklearn.cluster import DBSCAN
 from VisumPy.helpers import SetMulti
 
 
-def addConnections(_selCon, _tWalk):
-    _meter, _seconds = [], []
+def addConnections(_selCon, _tWalk, _shares = False):
+    _meter, _seconds, _weights = [], [], []
     _nPrT = 0
     Visum.Net.Connectors.SetPassive()
     for index, row in _selCon.iterrows():
@@ -22,13 +23,22 @@ def addConnections(_selCon, _tWalk):
         _con = Visum.Net.AddConnector(row["ZONE"], row["STOPAREA"])
         _con.Active = True
         _meter.extend([row["avg_meter"] / 1000] * 2)
-        _seconds.extend([_round30(row["avg_meter"] / (_tWalk / 3.6))] * 2)
+        if _shares:
+            _seconds.extend([240] * 2)
+            _weights.extend([row["weights"] * 100] * 2)
+        else:
+            _seconds.extend([_round30(row["avg_meter"] / (_tWalk / 3.6))] * 2)
+            _weights.extend([1] * 2)
     
     # Edit Attributes
     SetMulti(Visum.Net.Connectors, "TSYSSET", ["OEVFUSS"] * Visum.Net.Connectors.CountActive, True)
     SetMulti(Visum.Net.Connectors, "LENGTH", _meter, True)
     SetMulti(Visum.Net.Connectors, "T0_TSYS(OEVFUSS)", _seconds, True)
+    SetMulti(Visum.Net.Connectors, "WEIGHT(PUT)", _weights, True)
     Visum.Net.Connectors.SetActive()
+    
+    if _shares: SetMulti(Visum.Net.Zones, "SHAREPUT", [1] * Visum.Net.Zones.CountActive, True)
+    else: SetMulti(Visum.Net.Zones, "SHAREPUT", [0] * Visum.Net.Zones.CountActive, True)
     
     Visum.Log(20480, f"{(len(_selCon)-_nPrT) * 2} new PT-Connectors added")
     return
@@ -94,30 +104,42 @@ def poi2udt(_POICat, _UDTName):
     Visum.Log(20480, "Attributes merged")
     return
 
-def StopAreas2Connect(_attrCon):
+def StopAreas2Connect(_attrCon, _shares = False):
     
-    # at least 15% of potential
+    # at least 15% of max potential
     _attrCon = _attrCon[(_attrCon['pot_con'] / _attrCon['pot_max']) >= 0.15]
     
     # at Least 120% potential of neutral StopArea (TYPE 8)
     max8 = _attrCon[_attrCon["TYPENO"]==8]
     max8 = max8.rename(columns={'pot_con': 'pot_8'})
-    _attrCon = pd.merge(_attrCon, max8[['pot_8', 'ZONE', 'STOPNO']], on = ['ZONE', 'STOPNO'], how = "left")
+    _attrCon = pd.merge(_attrCon, max8[['pot_8', 'ZONE', 'STOPNO', 'cluster']], on = ['ZONE', 'STOPNO', 'cluster'], how = "left")
     _attrCon['pot_8'] = _attrCon['pot_8'].fillna(1)
     _attrCon = _attrCon[((_attrCon['pot_con'] / _attrCon['pot_8']) >= 1.2) | (_attrCon['TYPENO'] == 8)]
     
+    # at least 20% of max vehicle journeys
+    if _shares:
+        _attrCon['nVJmax'] = _attrCon.groupby(['ZONE', "cluster"])['nVJ'].transform('max')
+        _attrCon = _attrCon[(_attrCon['nVJ'] / _attrCon['nVJmax']) >= 0.2]
+        _attrCon['pot_con_sum'] = _attrCon.groupby(['ZONE', "cluster"])['pot_con'].transform('sum')
+        _attrCon["weights"] = (_attrCon["cluster_pot_sum"] / _attrCon["cluster_pot_uniqueSum"]) * (_attrCon["pot_con"] / _attrCon["pot_con_sum"])
+        _attrCon["avg_meter_sum"] = _attrCon["avg_meter"] * _attrCon["weights"]
+        _attrCon = _attrCon.groupby(["ZONE", "STOPAREA"], as_index=False).agg({'weights': 'sum', 'avg_meter_sum': 'sum'})
+        _attrCon["avg_meter"] = _attrCon['avg_meter_sum'] / _attrCon['weights']
+    
     # avoid multiple cons to same lines
-    _linesPot = _attrCon.explode('LINES')
-    _linesPot = _linesPot.groupby(['ZONE', 'LINES'], as_index = False)['pot_con'].max()
-    _attrCon["pot_lines"] = _attrCon.apply(_pot2Lines, _lines2stoparea = _linesPot, axis=1)
-    _attrCon = _attrCon[_attrCon["pot_lines"] == 1]
+    if not _shares:
+        _linesPot = _attrCon.explode('LINES')
+        _linesPot = _linesPot.groupby(['ZONE', 'LINES'], as_index = False)['pot_con'].max()
+        _attrCon["pot_lines"] = _attrCon.apply(_pot2Lines, _lines2stoparea = _linesPot, axis=1)
+        _attrCon = _attrCon[_attrCon["pot_lines"] == 1]
     
     Visum.Log(20480, "StopAreas to be connected have been identified")
     return _attrCon
 
-def weights2Connections(_UDTName, _pot_fact, _maxDist = 1500):
+def weights2Connections(_UDTName, _pot_fact, _shares = False, _maxDist = 1500):
     _zones = pd.DataFrame(Visum.Net.Zones.GetMultipleAttributes(["NO"], True))
     _zones.columns = ["NO"]
+    Visum.Log(20480, f"Creating new PT-Connectors for {len(_zones)} Zones")
     
     udt = Visum.Net.TableDefinitions.ItemByKey(_UDTName)
     df_udt = pd.DataFrame(udt.TableEntries.GetMultipleAttributes(
@@ -125,33 +147,53 @@ def weights2Connections(_UDTName, _pot_fact, _maxDist = 1500):
     df_udt.columns = ["NO", "CODE", "STOPAREA", "METER", "POTENTIAL", "ZONE", "X", "Y"]
     df_udt = df_udt[df_udt["ZONE"].isin(_zones["NO"])]
     
-    # weights
-    df_udt["pot_con"] = df_udt["POTENTIAL"] * df_udt["CODE"].map(_pot_fact)
-    df_udt["pot_con"] = ((1 - df_udt["METER"] / _maxDist)**3) * df_udt["pot_con"]
+    # weights and cluster
+    df_udt["pot_sum"] = df_udt["POTENTIAL"] * df_udt["CODE"].map(_pot_fact)
+    df_udt["pot_con"] = ((1 - df_udt["METER"] / _maxDist)**3) * df_udt["pot_sum"]
     df_udt["pot_meter"] = df_udt["METER"] * df_udt["pot_con"]
     df_udt["pot_x"] = df_udt["X"] * df_udt["pot_con"]
     df_udt["pot_y"] = df_udt["Y"] * df_udt["pot_con"]
+    aggr = {'pot_con': 'sum', 'pot_meter': 'sum', 'pot_x': 'sum', 'pot_y': 'sum'}
+    df_udt["cluster"] = 1
     
-    df_udt = df_udt.groupby(["ZONE", "STOPAREA"], as_index=False).agg({
-        "pot_con": "sum", "pot_meter": "sum", "pot_x": "sum", "pot_y": "sum"})
-    df_udt['pot_max'] = df_udt.groupby('ZONE')['pot_con'].transform('max')
+    if _shares:
+        df_udt = df_udt.groupby('ZONE', group_keys = False).apply(lambda z: _clusterXY(z, z.name),include_groups = False)
+        df_udt["cluster_pot_sum"] = df_udt.groupby(['ZONE', "cluster"])['pot_sum'].transform('sum')
+        df_udt["cluster_pot_max"] = df_udt.groupby('ZONE')['cluster_pot_sum'].transform('max')
+        df_udt = df_udt[(df_udt["cluster_pot_max"] != 0) & (df_udt["cluster_pot_sum"] / df_udt["cluster_pot_max"] >= 0.1) & (df_udt["cluster"] != -1)]
+        df_udt["cluster_n"] = df_udt.groupby('ZONE')['cluster'].transform('nunique')
+        # df_udt["cluster_pot_uniqueSum"] = df_udt["cluster_pot_sum"].unique().sum()
+        df_udt["cluster_pot_uniqueSum"] = df_udt.groupby("ZONE")["cluster_pot_sum"].transform(lambda x: x.unique().sum())
+        aggr.update({'cluster_pot_sum': 'first', 'cluster_pot_max': 'first', "cluster_pot_uniqueSum": "first", 'cluster_n': 'first'})
+        
+    df_udt = df_udt.groupby(["ZONE", "STOPAREA", "cluster"], as_index=False).agg(aggr)
+    df_udt['pot_max'] = df_udt.groupby(['ZONE', "cluster"])['pot_con'].transform('max')
     df_udt["avg_meter"] = df_udt["pot_meter"] / df_udt["pot_con"]
         
     # StopAreas    
     df_stoparea = pd.DataFrame(Visum.Net.StopAreas.GetMultipleAttributes(
         ["NO", "STOPNO", "TYPENO", r"MIN:STOPPOINTS\DISTINCT:SERVINGVEHJOURNEYS\LINENAME",
-         r"STOP\DISTINCT:STOPAREAS\DISTINCT:STOPPOINTS\DISTINCT:SERVINGVEHJOURNEYS\LINENAME"], False))
-    df_stoparea.columns = ["NO", "STOPNO", "TYPENO", "LINES", "LINES_STOP"]
+         r"STOP\DISTINCT:STOPAREAS\DISTINCT:STOPPOINTS\DISTINCT:SERVINGVEHJOURNEYS\LINENAME",
+         r"SUM:STOPPOINTS\COUNT:SERVINGVEHJOURNEYS"], False))
+    df_stoparea.columns = ["NO", "STOPNO", "TYPENO", "LINES", "LINES_STOP", "nVJ"]
     df_stoparea.loc[df_stoparea['TYPENO'] == 8, 'LINES'] = df_stoparea['LINES_STOP']
     df_stoparea.drop('LINES_STOP', axis = 1, inplace = True)
     df_stoparea = df_stoparea[~((df_stoparea['LINES'] == "") & (df_stoparea['TYPENO'] != 8))] # no stops without lines and TypNo != 8
     df_stoparea['LINES'] = df_stoparea['LINES'].str.split('|')
+    df_stoparea['nLines'] = df_stoparea['LINES'].str.len()
     
     # merge
     df_merged = pd.merge(df_udt, df_stoparea, left_on = "STOPAREA", right_on = "NO", how = "left")
     
     Visum.Log(20480, "Connections weighted and ready for calculations")
     return df_merged
+
+def _clusterXY(_dfXY, _zone, _eps = 150, _min_samples = 2):
+    _dfXY["ZONE"] = _zone
+    XY = _dfXY[['X', 'Y']]
+    model = DBSCAN(eps = _eps, min_samples = _min_samples)
+    _dfXY['cluster'] = model.fit_predict(XY)
+    return _dfXY
 
 def _pot2Lines(data, _lines2stoparea):
     if data["TYPENO"] == 8:
@@ -167,10 +209,9 @@ def _round30(n):
 
 if calpoi2udt:
     poi2udt(POICat, UDTName)
-attrCon = weights2Connections(UDTName, pot_fact)
-selCon = StopAreas2Connect(attrCon)
+attrCon = weights2Connections(UDTName, pot_fact, shares)
+selCon = StopAreas2Connect(attrCon, shares)
 delConnections(selCon)
-addConnections(selCon, tWalk)
-if calpoi2Center:
-    poi2Center(attrCon)
+addConnections(selCon, tWalk, shares)
+poi2Center(attrCon)
 Visum.Log(20480, "Finished")
