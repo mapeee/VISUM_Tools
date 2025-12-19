35cf01289eefabfe5d0645793fd28e0d8ab3de69
mapeee <marcus_peter@gmx.de>
2025-12-19 16:46:03 +0100

    - TNM_UDA: new version for final TNM calculation

--- 02Analysis/TNM/TNM_UDA.py
+++ 02Analysis/TNM/TNM_UDA.py
@@ -3,36 +3,55 @@
 Created on Thu Aug 28 20:14:40 2025
 
 @author: peter
-"""    
-Gebiete = ["FHH", "PI", "SE"]
-Saisons = ["S", "F"]
-FZG = ["SB", "GB"]
-Einheiten = ["KM", "STD"]
+"""
 
+import pandas as pd
 
-def createUDA(_name, _group, _type):
+def run_UDA(Visum):
+    Gebiete = _get_territories(Visum)
+    Saisons = ["S", "F"]
+    FZG = _get_vehicleCombinations(Visum)
+    Einheiten = ["KM", "STD"]
+    
+    # UDG
+    UDGs = Visum.Net.UserDefinedGroups.GetMultiAttValues("NAME")
+    for g, gname in Gebiete:
+        if not any(f"TNM_{g}" in UDG for _, UDG in UDGs):
+            Visum.Net.AddUserDefinedGroup(f"TNM_{g}", f"TNM {gname}")
+        if not any(f"TNM_{g}_FZG" in UDG for _, UDG in UDGs):
+            Visum.Net.AddUserDefinedGroup(f"TNM_{g}_FZG", f"TNM {gname} (Fahrzeugbedarf)")
+    
+    # UDA Vehicles
+    for s in Saisons:
+        for f in FZG:
+            _createUDA(Visum, f"{s}_{f}_M1", "TNM_FZG", [2, 6]) # Fahrzuegbedarf je Linie: Methode 1
+            _createUDA(Visum, f"{s}_{f}_M2", "TNM_FZG", [1, 0]) # Fahrzuegbedarf je Linie: Methode 2
+            _createUDA(Visum, f"{s}_{f}_STD", "TNM_FZG", [2, 6]) # Stunden je Fahrzeug je Linie
+            for g, gname in Gebiete:
+                _createUDA(Visum, f"{g}_{s}_{f}_M1", f"TNM_{g}_FZG", [2, 6]) # Fahrzuegbedarf je Linie: Methode 1
+                _createUDA(Visum, f"{g}_{s}_{f}_M2", f"TNM_{g}_FZG", [1, 0]) # Fahrzuegbedarf je Linie: Methode 2
+                for e in Einheiten:
+                    _createUDA(Visum, f"{g}_{s}_{f}_{e}", f"TNM_{g}", [2, 6])
+                    
+    Visum.Log(20480, "TNM-Rechenattribute (BDA): erzeugt")
+
+def _createUDA(Visum, _name, _group, _type):
     if not Visum.Net.Lines.AttrExists(_name):
         Visum.Net.Lines.AddUserDefinedAttribute(_name, _name, _name, _type[0], _type[1], False, 0, None, 0, SubAttr = 'TISET_1')
         UDA = Visum.Net.Lines.Attributes.ItemByKey(_name)
+        UDA.Comment = "TNM-Rechenattribut"
         UDA.UserDefinedGroup = _group
     
-# UDG
-UDGs = Visum.Net.UserDefinedGroups.GetMultiAttValues("NAME")
-for g in Gebiete:
-    if not any(f"TNM_{g}" in UDG for _, UDG in UDGs):
-        Visum.Net.AddUserDefinedGroup(f"TNM_{g}")
-    if not any(f"TNM_{g}_FZG" in UDG for _, UDG in UDGs):
-        Visum.Net.AddUserDefinedGroup(f"TNM_{g}_FZG")
+def _get_territories(Visum):
+    df_t_put = pd.DataFrame(Visum.Net.TerritoryPuTDetails.GetMultipleAttributes([r"TERRITORYCODE", "TERRITORYNAME"], True),
+                            columns = ["CODE", "NAME"])
+    t_unique = df_t_put[['CODE', "NAME"]].drop_duplicates().values.tolist()
+    return t_unique
 
-# UDA Vehicles
-for s in Saisons:
-    for f in FZG:
-        createUDA(f"{s}_{f}_M1", "TNM_FZG", [2, 6]) # Fahrzuegbedarf je Linie: Methode 1
-        createUDA(f"{s}_{f}_M2", "TNM_FZG", [1, 0]) # Fahrzuegbedarf je Linie: Methode 2
-        createUDA(f"{s}_{f}_STD", "TNM_FZG", [2, 6]) # Stunden je Fahrzeug je Linie
-        for g in Gebiete:
-            createUDA(f"{g}_{s}_{f}_M1", f"TNM_{g}_FZG", [2, 6]) # Fahrzuegbedarf je Linie: Methode 1
-            createUDA(f"{g}_{s}_{f}_M2", f"TNM_{g}_FZG", [1, 0]) # Fahrzuegbedarf je Linie: Methode 2
-            for e in Einheiten:
-                createUDA(f"{g}_{s}_{f}_{e}", f"TNM_{g}", [2, 6])
+def _get_vehicleCombinations(Visum):
+    df_vj = pd.DataFrame(Visum.Net.VehicleJourneySections.GetMultipleAttributes([r"VEHCOMB\CODE"], True), columns = ["FZG"])
+    vc_unique = df_vj['FZG'].drop_duplicates().tolist()
+    return vc_unique
+
+
             
