2f210e2c1f23d20b9757f10c982ad4f0c58b715a
mapeee <marcus_peter@gmx.de>
2025-12-25 17:33:06 +0100

    - TNM_Filiter: Initial Version

---
+++ 02Analysis/TNM/utils/TNM_Filter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Filter alle relevanten Netzelemente auf das aktive Teilnetz.
+Linien und Gebiete.
+
+Erstellt: 25.12.2025
+@author: mape
+"""
+
+def NEFilter(Visum):
+    TN = Visum.Net.AttValue("TN")
+    Visum.Filters.InitAll()
+    Lines = Visum.Filters.LineGroupFilter()
+    LineFilter = Lines.LineFilter()
+    LineFilter.AddCondition("OP_NONE", False, "TN", "EqualVal", TN)
+    Lines.UseFilterForLines = True
+    Lines.UseFilterForLineRoutes = True
+    Lines.UseFilterForLineRoutes = True
+    Lines.UseFilterForLineRouteItems = True
+    Lines.UseFilterForTimeProfiles = True
+    Lines.UseFilterForTimeProfileItems = True
+    Lines.UseFilterForVehJourneys = True
+    Lines.UseFilterForVehJourneySections = True
+    Lines.UseFilterForVehJourneyItems = True
+    if Visum.Net.Lines.CountActive == 0:
+        Visum.Log(12288, f"{TN} gefiltert: keine Linie vorhanden")
+    else:
+        Territories = Visum.Filters.TerritoryFilter()
+        Territories.AddCondition("OP_NONE", False, "TYPENO", "ContainedIn", 1)
+        Visum.Log(20480, f"Filter gesetzt auf TN: {TN}")
+
+NEFilter(Visum)
