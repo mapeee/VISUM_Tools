89925e597cf8faefb74972b0858bb13088e0be73
mapeee <marcus_peter@gmx.de>
2026-01-05 16:59:08 +0100

    - TNM_Linienkennzahlen: all as functions

--- 02Analysis/TNM/TNM_Linienkennzahlen.py
+++ 02Analysis/TNM/TNM_Linienkennzahlen.py
@@ -6,9 +6,65 @@ Created on Thu Aug 28 20:29:11 2025
 
 import pandas as pd
 from VisumPy.helpers import SetMulti
-from utils.TNM_Checks import check_TN
-from utils.TNM_UDA import create_UDA
+from create.TNM_UDA import create_UDA
+from utils.TNM_Checks import check_timeIntervalls, check_BDA
 
+
+def main(Visum):
+    if not checks(Visum):
+        return  False
+    create_UDA(Visum)
+    Gebiete = get_territories()
+    Saisons = ["S", "F"]
+    Tage = ["AP", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+    Einheiten = ["KM", "STD"]
+    FZG = get_vehicleCombinations()
+
+    PTD = pd.DataFrame(Visum.Net.TerritoryPuTDetails.GetMultipleAttributes(["TERRITORYCODE", "LINENAME", "VEHCOMBCODE",
+                                                                            "SERVICEKM(AP)", "SERVICETIME(AP)",
+                                                                            "SERVICEKM(MO)", "SERVICEKM(DI)",
+                                                                            "SERVICEKM(MI)", "SERVICEKM(DO)",
+                                                                            "SERVICEKM(FR)", "SERVICEKM(SA)",
+                                                                            "SERVICEKM(SO)",
+                                                                            "SERVICETIME(MO)", "SERVICETIME(DI)",
+                                                                            "SERVICETIME(MI)", "SERVICETIME(DO)",
+                                                                            "SERVICETIME(FR)", "SERVICETIME(SA)",
+                                                                            "SERVICETIME(SO)",
+                                                                            r"VEHJOURNEY\MIN:VEHJOURNEYSECTIONS\S",
+                                                                            r"VEHJOURNEY\MIN:VEHJOURNEYSECTIONS\F"], True))
+    PTD.columns = ["Gebiet", "LINE", "FZG", "APKM", "APSTD", "MoKM", "DiKM", "MiKM", "DoKM", "FrKM", "SaKM", "SoKM",
+                   "MoSTD", "DiSTD", "MiSTD", "DoSTD", "FrSTD", "SaSTD", "SoSTD", "S", "F"]
+    PTD[["APSTD", "MoSTD", "DiSTD", "MiSTD", "DoSTD", "FrSTD", "SaSTD", "SoSTD"]] /= 3600
+
+    for s in Saisons:
+        PTDs = PTD[PTD[s] == 1]
+        for f in FZG:
+            PTDf = PTDs[PTDs["FZG"] == f]
+            for t in Tage:
+                Kenzahl = f"{s}_{f}_STD({t})"
+                edict = PTDf.groupby("LINE")[f"{t}STD"].sum().to_dict()
+                SetMulti(Visum.Net.Lines, Kenzahl, [edict.get(line, 0) for _, line in Visum.Net.Lines.GetMultiAttValues("NAME",True)], True)
+                for g in Gebiete:
+                    PTDg = PTDf[PTDf["Gebiet"] == g]
+                    for e in Einheiten:
+                        Kenzahl = f"{g}_{s}_{f}_{e}({t})"
+                        edict = PTDg.groupby("LINE")[f"{t}{e}"].sum().to_dict()
+                        SetMulti(Visum.Net.Lines, Kenzahl, [edict.get(line, 0) for _, line in Visum.Net.Lines.GetMultiAttValues("NAME",True)], True)
+                        
+    Visum.Log(20480, "Linienkennzahlen: berechnet")
+    
+
+def checks(Visum):
+    if Visum.Net.VehicleJourneySections.CountActive == 0:
+        Visum.Log(12288, "Keine aktiven FahrpanfahrtAbschnitte vorhanden")
+        return False
+    if not check_BDA(Visum, Visum.Net.VehicleJourneySections, "FahrpanfahrtAbschnitte", "S"):
+        return False
+    if not check_BDA(Visum, Visum.Net.VehicleJourneySections, "FahrpanfahrtAbschnitte", "F"):
+        return False
+    if not check_timeIntervalls(Visum):
+        return False
+    return True
 
 def get_territories():
     df_t_put = pd.DataFrame(Visum.Net.TerritoryPuTDetails.GetMultipleAttributes([r"TERRITORYCODE"], True),
@@ -21,49 +77,5 @@ def get_vehicleCombinations():
     vc_unique = df_vj['FZG'].drop_duplicates().tolist()
     return vc_unique
 
-###Checks######
-check_TN(Visum)
-###############
-
-###Create UDAs###
-create_UDA(Visum)
-#################
-
-Gebiete = get_territories()
-Saisons = ["S", "F"]
-Tage = ["AP", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
-Einheiten = ["KM", "STD"]
-FZG = get_vehicleCombinations()
-
-PTD = pd.DataFrame(Visum.Net.TerritoryPuTDetails.GetMultipleAttributes(["TERRITORYCODE", "LINENAME", "VEHCOMBCODE",
-                                                                        "SERVICEKM(AP)", "SERVICETIME(AP)",
-                                                                        "SERVICEKM(MO)", "SERVICEKM(DI)",
-                                                                        "SERVICEKM(MI)", "SERVICEKM(DO)",
-                                                                        "SERVICEKM(FR)", "SERVICEKM(SA)",
-                                                                        "SERVICEKM(SO)",
-                                                                        "SERVICETIME(MO)", "SERVICETIME(DI)",
-                                                                        "SERVICETIME(MI)", "SERVICETIME(DO)",
-                                                                        "SERVICETIME(FR)", "SERVICETIME(SA)",
-                                                                        "SERVICETIME(SO)",
-                                                                        r"VEHJOURNEY\MIN:VEHJOURNEYSECTIONS\S",
-                                                                        r"VEHJOURNEY\MIN:VEHJOURNEYSECTIONS\F"], True))
-PTD.columns = ["Gebiet", "LINE", "FZG", "APKM", "APSTD", "MoKM", "DiKM", "MiKM", "DoKM", "FrKM", "SaKM", "SoKM",
-               "MoSTD", "DiSTD", "MiSTD", "DoSTD", "FrSTD", "SaSTD", "SoSTD", "S", "F"]
-PTD[["APSTD", "MoSTD", "DiSTD", "MiSTD", "DoSTD", "FrSTD", "SaSTD", "SoSTD"]] /= 3600
-
-for s in Saisons:
-    PTDs = PTD[PTD[s] == 1]
-    for f in FZG:
-        PTDf = PTDs[PTDs["FZG"] == f]
-        for t in Tage:
-            Kenzahl = f"{s}_{f}_STD({t})"
-            edict = PTDf.groupby("LINE")[f"{t}STD"].sum().to_dict()
-            SetMulti(Visum.Net.Lines, Kenzahl, [edict.get(line, 0) for _, line in Visum.Net.Lines.GetMultiAttValues("NAME",True)], True)
-            for g in Gebiete:
-                PTDg = PTDf[PTDf["Gebiet"] == g]
-                for e in Einheiten:
-                    Kenzahl = f"{g}_{s}_{f}_{e}({t})"
-                    edict = PTDg.groupby("LINE")[f"{t}{e}"].sum().to_dict()
-                    SetMulti(Visum.Net.Lines, Kenzahl, [edict.get(line, 0) for _, line in Visum.Net.Lines.GetMultiAttValues("NAME",True)], True)
-                    
-Visum.Log(20480, "Linienkennzahlen: berechnet")
+if __name__ == "__main__":           
+    main(Visum)
