2bfd7a02555c8027d47158ac46ea36ef6bf2c5ae
mapeee <marcus_peter@gmx.de>
2026-01-12 15:53:26 +0100

    - TNM_Abrechnung: completely new

--- 02Analysis/TNM/TNM_Abrechnung.py
+++ 02Analysis/TNM/TNM_Abrechnung.py
@@ -1,106 +1,102 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep  1 09:47:03 2025
+Bereite Abrechnung auf Ebene FzgKombinationen auf
+FahrzeugKombinationen (Teilnetze)
 
-@author: peter
+Erstellt: 09.01.2026
+@author: mape
 """
 
+import numpy as np
 import pandas as pd
 from VisumPy.helpers import SetMulti
+from create.TNM_UDA import create_UDA_FZGAbr
+from utils.TNM_Checks import check_timeIntervalls, check_BDA, check_vehiclejournyeSections
 
-Teilnetze = ["PI 1-3"]
-Parameter = pd.DataFrame(Visum.Net.TableDefinitions.ItemByKey("Parameter").TableEntries.GetMultipleAttributes(["KENNZAHL", "WERT"]), columns = ["KENNZAHL", "WERT"])
+
+# import win32com.client.dynamic
+# Visum = win32com.client.dynamic.Dispatch("Visum.Visum.25")
+# Visum.IO.loadversion(r"C:\Users\peter\hvv.de\B-GR-Bereich B - Planungstool hvv\VisumTest\VisumTest_neu.ver")
+
+# Parameter
+S = 40
+F = 12
+M = "M1"
 
 
-def calculateAbrechnung(_TN, _Gebiete, _Parameter):
-    AbrechnungTable = _getAbrechnungTable(_TN)
-    LinesTable = _getLinesTable(_TN)
-    MengenTable = pd.DataFrame(Visum.Net.TableDefinitions.ItemByKey(f"{_TN} Mengen").TableEntries.GetMultipleAttributes(["KENNZAHL","KM","STD"]))
-    MengenTable.columns = ["KENNZAHL", "KM", "STD"]
-    MengenTable["KENNZAHL2"] = MengenTable["KENNZAHL"].str.replace(r"_[^_]+$", "", regex=True)
+def main(Visum):
+    if not _checks(Visum):
+        return  False
+    TN = Visum.Net.AttValue("TN")
+    Gebiete = _get_territories(Visum)
+    Einheiten = [["KM", "Fahrplankilometer"], ["STD", "Fahrplanstunden"], ["FZG", "Fahrzeugbedarf"]]
+    FZG = _get_vehicleCombinations(Visum)
+    create_UDA_FZGAbr(Visum, TN, Gebiete, Einheiten)
     
-    FZG = _getFZG(_TN)
-    FZGtable = pd.DataFrame(Visum.Net.TableDefinitions.ItemByKey(f"{_TN} Fahrzeugbedarf").TableEntries.GetMultipleAttributes(["TYP"] + FZG))
-    FZGtable.columns = ["TYP"] + FZG
-    FZGtable = FZGtable[FZGtable["TYP"] == "Bedarf"]
-    for f in FZG:
-        FZGtable[f] = FZGtable[f].astype(int)
-    
-    Schulwochen = int(_Parameter[_Parameter["KENNZAHL"] == "Schulwochen"]["WERT"].iloc[0])
-    Ferienwochen = int(_Parameter[_Parameter["KENNZAHL"] == "Ferienwochen"]["WERT"].iloc[0])
-    TA_FZG = _Parameter[_Parameter["KENNZAHL"] == "TA_FZG"]["WERT"].iloc[0]
-    
-    
-    for index, row in AbrechnungTable.iterrows():
-        if row["KENNZAHL"][-3:] == "_KM":
-            FZG = row["KENNZAHL"].split("_")[0]
-            g_sum = 0
-            for g in _Gebiete:
-                S = MengenTable[MengenTable["KENNZAHL"]==f"{g}_S_{FZG}"]["KM"].iloc[0] * Schulwochen
-                F = MengenTable[MengenTable["KENNZAHL"]==f"{g}_F_{FZG}"]["KM"].iloc[0] * Ferienwochen
-                AbrechnungTable.at[index, f"{g}_MENGEN"] = S + F
-                g_sum += S + F
-            AbrechnungTable.at[index, "GESAMT_MENGEN"] = g_sum
-        elif row["KENNZAHL"] == "STD":
-            g_sum = 0
-            for g in _Gebiete:
-                S = MengenTable[MengenTable["KENNZAHL2"]==f"{g}_S"]["STD"].sum() * Schulwochen
-                F = MengenTable[MengenTable["KENNZAHL2"]==f"{g}_F"]["STD"].sum() * Ferienwochen
-                AbrechnungTable.at[index, f"{g}_MENGEN"] = S + F
-                g_sum += S + F
-            AbrechnungTable.at[index, "GESAMT_MENGEN"] = g_sum
-        else:
-            AbrechnungTable.at[index, "GESAMT_MENGEN"] = FZGtable[row["KENNZAHL"]].max()
-            for g in _Gebiete:
-                pattern = rf"^{g}.*{row['KENNZAHL']}_{TA_FZG}.*$"
-                m_cols = LinesTable.loc[:, LinesTable.columns.str.match(pattern)]
-                col_sums = m_cols.sum(axis=0)
-                AbrechnungTable.at[index, f"{g}_MENGEN"] = col_sums.max()
-        
-    _editAbrechnungTable(_TN, _Gebiete, AbrechnungTable)
-    
+    LinesTable = _get_LinesTable(Visum, Gebiete, Einheiten, FZG, M)
+    FZGKomb(Visum, TN, LinesTable, Gebiete, Einheiten, FZG, S, F, M)
 
-def _editAbrechnungTable(_TN, _Gebiete, _AbrechnungTable):
-    for g in _Gebiete:
-        SetMulti(Visum.Net.TableDefinitions.ItemByKey(f"{_TN} Abrechnung").TableEntries, f"{g}_MENGEN", _AbrechnungTable[f"{g}_MENGEN"].tolist(), True)
-    SetMulti(Visum.Net.TableDefinitions.ItemByKey(f"{_TN} Abrechnung").TableEntries, "GESAMT_MENGEN", _AbrechnungTable["GESAMT_MENGEN"].tolist(), True)
-    
+def FZGKomb(Visum, _TN, _LinesTable, _Gebiete, _Einheiten, _FZG, _S, _F, _M):
+    df_vc = pd.DataFrame(Visum.Net.VehicleCombinations.GetMultipleAttributes(["CODE"], True), columns = ["FZG"])
+    for g, gname in _Gebiete:
+        for e, ename in _Einheiten:
+            data = []
+            for f in df_vc["FZG"]:
+                if f not in _FZG:
+                    total  = 0
+                elif e in ["KM", "STD"]:
+                    total = ((_LinesTable[f"{g}_S_{f}_{e}(AP)"] * _S) +
+                             (_LinesTable[f"{g}_F_{f}_{e}(AP)"] * _F)).sum()
+                else:
+                    cols = [c for c in _LinesTable.columns if "_S_" in c and _M in c and f in c and g in c]
+                    max_S = _LinesTable[cols].sum().max()
+                    cols = [c for c in _LinesTable.columns if "_F_" in c and _M in c and f in c and g in c]
+                    max_F = _LinesTable[cols].sum().max()
+                    total = max(max_S, max_F)
+                data.append(total)
+            SetMulti(Visum.Net.VehicleCombinations, f"{_TN}_{g}_{e}", data)
 
-def _getAbrechnungTable(_TN):
+def _checks(Visum):
+    if Visum.Net.VehicleJourneySections.CountActive == 0:
+        Visum.Log(12288, "Keine aktiven FahrpanfahrtAbschnitte vorhanden")
+        return False
+    if not check_BDA(Visum, Visum.Net, "Network", "TN"):
+        return False
+    TN = Visum.Net.AttValue("TN")
+    if not check_BDA(Visum, Visum.Net.Lines, "Lines", "TN"):
+        return False
+    if not check_BDA(Visum, Visum.Net.VehicleCombinations, "VehicleCombinations", f"{TN}_FZG_METN"):
+        return False
+    if not check_timeIntervalls(Visum):
+        return False
+    if not check_vehiclejournyeSections(Visum, True):
+        return False
+    return True
+
+def _get_territories(Visum):
+    df_t_put = pd.DataFrame(Visum.Net.VehicleJourneyItems.GetMultipleAttributes([r"TIMEPROFILEITEM\LINEROUTEITEM\NODE\MINACTIVE:CONTAININGTERRITORIES\CODE",
+                                                                                  r"TIMEPROFILEITEM\LINEROUTEITEM\NODE\MINACTIVE:CONTAININGTERRITORIES\NAME"], True),
+                            columns = ["CODE", "NAME"])
+    t_unique = df_t_put[['CODE', "NAME"]].drop_duplicates().values.tolist()
+    return t_unique
+
+def _get_vehicleCombinations(Visum):
+    df_vc = pd.DataFrame(Visum.Net.VehicleJourneySections.GetMultipleAttributes([r"VEHCOMB\CODE"], True),
+                            columns = ["FZG"])
+    vc_unique = df_vc['FZG'].drop_duplicates().values.tolist()
+    return vc_unique
+    
+def _get_LinesTable(Visum, _Gebiete, _Einheiten, _FZG, _M):
     attrList = []
-    for i in Visum.Net.TableDefinitions.ItemByKey(f"{_TN} Abrechnung").TableEntries.Attributes.GetAll:
-        if i.IsUserDefined and i.Editable and i.ID != "VERRECHNUNGSSATZ":
-            attrList.append(i.ID)
-    _AbrechnungTable = pd.DataFrame(Visum.Net.TableDefinitions.ItemByKey(f"{_TN} Abrechnung").TableEntries
-                                     .GetMultipleAttributes(attrList, True), columns = attrList)
-    return _AbrechnungTable
-
-
-def _getFZG(_TN):
-    _FZGTable = pd.DataFrame(Visum.Net.TableDefinitions.ItemByKey("Teilnetze").TableEntries
-                                     .GetMultipleAttributes(["TEILNETZGRUPPE", "FAHRZEUGE"], True), columns = ["TEILNETZGRUPPE", "FAHRZEUGE"])
-    _FZG = _FZGTable[_FZGTable["TEILNETZGRUPPE"] == _TN]["FAHRZEUGE"].tolist()
-    _FZG = _FZG[0].split(", ")
-
-    return _FZG
-
-
-def _getGebiete(_TN):
-    _GebieteTable = pd.DataFrame(Visum.Net.TableDefinitions.ItemByKey("Teilnetze").TableEntries
-                                     .GetMultipleAttributes(["TEILNETZGRUPPE", "GEBIETE"], True), columns = ["TEILNETZGRUPPE", "GEBIETE"])
-    _Gebiete = _GebieteTable[_GebieteTable["TEILNETZGRUPPE"] == _TN]["GEBIETE"].tolist()
-    _Gebiete = _Gebiete[0].split(", ")
-    return _Gebiete
-
-def _getLinesTable(_TN):
-    attrList = [f"{x.ID}({suffix})" for x in Visum.Net.Lines.Attributes.GetAll 
-                if any(tag in x.ID for tag in ("M1", "M2")) 
-                for suffix in ("Mo", "Di", "Mi", "Do", "Fr", "Sa", "So")]
-    attrList.append("TEILNETZGRUPPE")
+    for g, _ in _Gebiete:
+        for e, _ in _Einheiten:
+            for f in _FZG:
+                times = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"] if e == "FZG" else ["AP"]
+                e_val = _M if e == "FZG" else e
+                for t in times:
+                    attrList.extend([f"{g}_S_{f}_{e_val}({t})", f"{g}_F_{f}_{e_val}({t})",])
     _LinesTable = pd.DataFrame(Visum.Net.Lines.GetMultipleAttributes(attrList, True), columns = attrList)
-    _LinesTable = _LinesTable[_LinesTable["TEILNETZGRUPPE"] == _TN]
     return _LinesTable
-
-for TN in Teilnetze:
-    Gebiete = _getGebiete(TN)
-    calculateAbrechnung(TN, Gebiete, Parameter)
+    
+if __name__ == "__main__":           
+    main(Visum)
